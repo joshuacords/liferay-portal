f29247d3d03a9082f3a67650b9f8ef5422b473e5
Joshua Cords <joshua.cords@liferay.com>
2017-02-10 15:12:55 -0800

    LPS-70427 Added Queries for findByGroupRoleAndTeamRoleAndRoleIds. Added method to return all Active roles for a portlet

--- portal-impl/src/com/liferay/portal/util/ResourcePermissionUtil.java
+++ portal-impl/src/com/liferay/portal/util/ResourcePermissionUtil.java
@@ -14,14 +14,19 @@
 
 package com.liferay.portal.util;
 
+import com.liferay.portal.kernel.dao.orm.DynamicQuery;
+import com.liferay.portal.kernel.dao.orm.DynamicQueryFactoryUtil;
+import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Resource;
 import com.liferay.portal.kernel.model.ResourceBlock;
 import com.liferay.portal.kernel.model.ResourceConstants;
+import com.liferay.portal.kernel.model.ResourcePermission;
 import com.liferay.portal.kernel.model.Role;
 import com.liferay.portal.kernel.model.RoleConstants;
 import com.liferay.portal.kernel.service.ResourceBlockLocalServiceUtil;
 import com.liferay.portal.kernel.service.ResourcePermissionLocalServiceUtil;
+import com.liferay.portal.kernel.util.PortalClassLoaderUtil;
 
 import java.util.List;
 
@@ -30,6 +35,30 @@ import java.util.List;
  * @author Sergio González
  */
 public class ResourcePermissionUtil {
+
+	public static List<ResourcePermission> getActiveResourcePermissions(
+		long companyId, String name, int scope, String primKey) {
+
+		DynamicQuery resoucePermissionQuery = DynamicQueryFactoryUtil.forClass(
+			ResourcePermission.class, PortalClassLoaderUtil.getClassLoader());
+
+		resoucePermissionQuery.add(
+			PropertyFactoryUtil.forName("companyId").eq(companyId));
+		resoucePermissionQuery.add(
+			PropertyFactoryUtil.forName("scope").eq(scope));
+		resoucePermissionQuery.add(
+			PropertyFactoryUtil.forName("name").eq(name));
+		resoucePermissionQuery.add(
+			PropertyFactoryUtil.forName("primKey").eq(primKey));
+		resoucePermissionQuery.add(
+			PropertyFactoryUtil.forName("actionIds").ne(Long.valueOf("0")));
+
+		List<ResourcePermission> results =
+			ResourcePermissionLocalServiceUtil.dynamicQuery(
+				resoucePermissionQuery);
+
+		return results;
+	}
 
 	public static void populateResourcePermissionActionIds(
 			long groupId, Role role, Resource resource, List<String> actions,
