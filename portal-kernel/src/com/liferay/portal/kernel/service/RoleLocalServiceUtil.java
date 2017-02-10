dc381819afba8da3e539451b627fb57cc2ff607d
Joshua Cords <joshua.cords@liferay.com>
2017-02-10 14:18:50 -0800

    LPS-70427 Ran Build-Service

--- portal-kernel/src/com/liferay/portal/kernel/service/RoleLocalServiceUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/service/RoleLocalServiceUtil.java
@@ -14,10 +14,12 @@
 
 package com.liferay.portal.kernel.service;
 
-import aQute.bnd.annotation.ProviderType;
-
 import com.liferay.portal.kernel.bean.PortalBeanLocatorUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.RoleConstants;
 import com.liferay.portal.kernel.util.ReferenceRegistry;
+
+import aQute.bnd.annotation.ProviderType;
 
 /**
  * Provides the local service utility for Role. This utility wraps
@@ -382,6 +384,17 @@ public class RoleLocalServiceUtil {
 		return getService().getAssigneesTotal(roleId);
 	}
 
+	public static int getGroupRolesAndTeamRolesAndRoleIdsCount(long companyId,
+		java.lang.String keywords,
+		java.util.List<java.lang.String> excludedNames, int[] types,
+		long excludedTeamRoleId, long teamGroupId, long[] roleIds)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		return getService()
+				   .getGroupRolesAndTeamRolesAndRoleIdsCount(companyId,
+			keywords, excludedNames, types, excludedTeamRoleId, teamGroupId,
+			roleIds);
+	}
+
 	public static int getGroupRolesAndTeamRolesCount(long companyId,
 		java.lang.String keywords,
 		java.util.List<java.lang.String> excludedNames, int[] types,
@@ -585,6 +598,17 @@ public class RoleLocalServiceUtil {
 		return getService()
 				   .getGroupRolesAndTeamRoles(companyId, keywords,
 			excludedNames, types, excludedTeamRoleId, teamGroupId, start, end);
+	}
+
+	public static java.util.List<com.liferay.portal.kernel.model.Role> getGroupRolesAndTeamRolesAndRoleIds(
+		long companyId, java.lang.String keywords,
+		java.util.List<java.lang.String> excludedNames, int[] types,
+		long excludedTeamRoleId, long teamGroupId, long[] roleIds, int start,
+		int end) throws com.liferay.portal.kernel.exception.PortalException {
+		return getService()
+				   .getGroupRolesAndTeamRolesAndRoleIds(companyId, keywords,
+			excludedNames, types, excludedTeamRoleId, teamGroupId, roleIds,
+			start, end);
 	}
 
 	public static java.util.List<com.liferay.portal.kernel.model.Role> getResourceBlockRoles(
