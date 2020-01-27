2b061d8eceaed2a67f401a6a3983d976dfe580d4
Pavel Savinov <pavel.savinov@liferay.com>
2020-01-27 11:45:10 +0100

    LPS-141715 Partial backport of LPS-107664: Use the "published" typeSetting to check if the draft has been approved since the publicationDate can also refer to a staging publish process and thus return inaccurate results. Original commit message: LPS-107664 Don't show pages in the navigation menu if they weren't published 34f0fe566d16b3c57ff1432031a8fe67059957f8

--- portal-kernel/src/com/liferay/portal/kernel/theme/NavItem.java
+++ portal-kernel/src/com/liferay/portal/kernel/theme/NavItem.java
@@ -23,8 +23,10 @@ import com.liferay.portal.kernel.model.LayoutType;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.service.LayoutLocalServiceUtil;
 import com.liferay.portal.kernel.service.permission.LayoutPermissionUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HtmlUtil;
 import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
@@ -398,6 +400,16 @@ public class NavItem implements Serializable {
 
 		if (Objects.equals(layout.getCreateDate(), layout.getPublishDate())) {
 			return true;
+		}
+
+		Layout draftLayout = LayoutLocalServiceUtil.fetchLayout(
+			PortalUtil.getClassNameId(Layout.class), layout.getPlid());
+
+		if (draftLayout != null) {
+			boolean published = GetterUtil.getBoolean(
+				draftLayout.getTypeSettingsProperty("published"));
+
+			return !published;
 		}
 
 		return false;
