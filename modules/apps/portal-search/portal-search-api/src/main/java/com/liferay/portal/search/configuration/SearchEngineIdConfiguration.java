a38afc0a61ccd5ce392aa3036c4000c671f39ee5
Joshua Cords <joshua.cords@liferay.com>
2020-03-18 16:08:38 -0700

    LPS-110420 portal-search-api: add SearchEngineIdConfiguration
    https://issues.liferay.com/browse/LPS-110420

---
+++ modules/apps/portal-search/portal-search-api/src/main/java/com/liferay/portal/search/configuration/SearchEngineIdConfiguration.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+import com.liferay.portal.kernel.search.SearchEngineHelper;
+
+import org.osgi.annotation.versioning.ProviderType;
+
+/**
+ * @author Joshua Cords
+ */
+@ExtendedObjectClassDefinition(category = "search")
+@Meta.OCD(
+	id = "com.liferay.portal.search.configuration.SearchEngineIdConfiguration",
+	localization = "content/Language",
+	name = "search-engine-id-configuration-name"
+)
+@ProviderType
+public interface SearchEngineIdConfiguration {
+
+	@Meta.AD(
+		deflt = SearchEngineHelper.SYSTEM_ENGINE_ID,
+		name = "search-engine-id", required = true
+	)
+	public String indexSearchEngineId();
+
+}
