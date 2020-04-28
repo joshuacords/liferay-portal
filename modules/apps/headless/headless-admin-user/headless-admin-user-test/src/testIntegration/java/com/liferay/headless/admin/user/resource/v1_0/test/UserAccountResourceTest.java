be8e0e184bb9060efbec615b92779020fba25c56
drewbrokke <drew.brokke@liferay.com>
2020-04-28 15:08:05 -0500

    LPS-111626 headless-admin-user-test - uses UserLocalService directly

--- modules/apps/headless/headless-admin-user/headless-admin-user-test/src/testIntegration/java/com/liferay/headless/admin/user/resource/v1_0/test/UserAccountResourceTest.java
+++ modules/apps/headless/headless-admin-user/headless-admin-user-test/src/testIntegration/java/com/liferay/headless/admin/user/resource/v1_0/test/UserAccountResourceTest.java
@@ -39,7 +39,7 @@ import com.liferay.portal.kernel.search.IndexWriterHelperUtil;
 import com.liferay.portal.kernel.search.Indexer;
 import com.liferay.portal.kernel.search.IndexerRegistryUtil;
 import com.liferay.portal.kernel.service.CompanyLocalServiceUtil;
-import com.liferay.portal.kernel.service.UserLocalServiceUtil;
+import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.OrganizationTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
@@ -50,6 +50,7 @@ import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.odata.entity.EntityField;
+import com.liferay.portal.test.rule.Inject;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -79,23 +80,23 @@ public class UserAccountResourceTest extends BaseUserAccountResourceTestCase {
 
 		_organization = OrganizationTestUtil.addOrganization();
 
-		_testUser = UserLocalServiceUtil.getUserByEmailAddress(
+		_testUser = _userLocalService.getUserByEmailAddress(
 			testGroup.getCompanyId(), "test@liferay.com");
 
-		UserLocalServiceUtil.deleteGroupUser(
+		_userLocalService.deleteGroupUser(
 			testGroup.getGroupId(), _testUser.getUserId());
 
 		// See LPS-94496 for why we have to delete all users except for the
 		// test user
 
-		List<User> users = UserLocalServiceUtil.getUsers(
+		List<User> users = _userLocalService.getUsers(
 			PortalUtil.getDefaultCompanyId(), false,
 			WorkflowConstants.STATUS_APPROVED, QueryUtil.ALL_POS,
 			QueryUtil.ALL_POS, null);
 
 		for (User user : users) {
 			if (user.getUserId() != _testUser.getUserId()) {
-				UserLocalServiceUtil.deleteUser(user);
+				_userLocalService.deleteUser(user);
 			}
 		}
 
@@ -399,7 +400,7 @@ public class UserAccountResourceTest extends BaseUserAccountResourceTestCase {
 		userAccount = _addUserAccount(
 			testGetSiteUserAccountsPage_getSiteId(), userAccount);
 
-		UserLocalServiceUtil.addOrganizationUser(
+		_userLocalService.addOrganizationUser(
 			GetterUtil.getLong(organizationId), userAccount.getId());
 
 		return userAccount;
@@ -457,9 +458,9 @@ public class UserAccountResourceTest extends BaseUserAccountResourceTestCase {
 
 		userAccount = userAccountResource.postUserAccount(userAccount);
 
-		_users.add(UserLocalServiceUtil.getUser(userAccount.getId()));
+		_users.add(_userLocalService.getUser(userAccount.getId()));
 
-		UserLocalServiceUtil.addGroupUser(siteId, userAccount.getId());
+		_userLocalService.addGroupUser(siteId, userAccount.getId());
 
 		return userAccount;
 	}
@@ -575,6 +576,9 @@ public class UserAccountResourceTest extends BaseUserAccountResourceTestCase {
 	private Organization _organization;
 
 	private User _testUser;
+
+	@Inject
+	private UserLocalService _userLocalService;
 
 	@DeleteAfterTestRun
 	private final List<User> _users = new ArrayList<>();
