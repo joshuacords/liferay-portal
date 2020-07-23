f96908aad7200fce2ec9a6ffc8eda96ba8a50284
Dante Wang <dante.wang@liferay.com>
2020-07-23 10:54:18 +0800

    LPS-116325 SF, merge test cases

--- modules/core/petra/petra-lang/src/test/java/com/liferay/petra/lang/ClassLoaderPoolTest.java
+++ modules/core/petra/petra-lang/src/test/java/com/liferay/petra/lang/ClassLoaderPoolTest.java
@@ -280,17 +280,6 @@ public class ClassLoaderPoolTest {
 	}
 
 	@Test
-	public void testUnregisterClassLoader() {
-		ClassLoaderPool.register(
-			_CONTEXT_NAME_WITHOUT_VERSION + "_X",
-			new URLClassLoader(new URL[0]));
-
-		ClassLoaderPool.unregister(_CONTEXT_NAME_WITHOUT_VERSION + "_X");
-
-		_assertEmptyMaps();
-	}
-
-	@Test
 	public void testUnregisterFallbackClassLoader() {
 		ClassLoaderPool.register(
 			_CONTEXT_NAME_1, new URLClassLoader(new URL[0]));
@@ -352,14 +341,18 @@ public class ClassLoaderPoolTest {
 
 	@Test
 	public void testUnregisterWithValidContextName() {
-		ClassLoader classLoader = new URLClassLoader(new URL[0]);
-
-		ClassLoaderPool.register(_CONTEXT_NAME_WITHOUT_VERSION, classLoader);
 		ClassLoaderPool.register(
-			_CONTEXT_NAME_WITHOUT_VERSION + "_", classLoader);
+			_CONTEXT_NAME_WITHOUT_VERSION, new URLClassLoader(new URL[0]));
+		ClassLoaderPool.register(
+			_CONTEXT_NAME_WITHOUT_VERSION + "_",
+			new URLClassLoader(new URL[0]));
+		ClassLoaderPool.register(
+			_CONTEXT_NAME_WITHOUT_VERSION + "_X",
+			new URLClassLoader(new URL[0]));
 
 		ClassLoaderPool.unregister(_CONTEXT_NAME_WITHOUT_VERSION);
 		ClassLoaderPool.unregister(_CONTEXT_NAME_WITHOUT_VERSION + "_");
+		ClassLoaderPool.unregister(_CONTEXT_NAME_WITHOUT_VERSION + "_X");
 
 		_assertEmptyMaps();
 	}
