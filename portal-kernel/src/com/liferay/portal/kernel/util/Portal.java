dad9f1d7f145296a463df31cf23e421f580588f2
Eric Yan <eric.yan@liferay.com>
2020-09-14 11:05:16 -0700

    LPS-88338 Add method to determine if a request was forwarded

--- portal-kernel/src/com/liferay/portal/kernel/util/Portal.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/Portal.java
@@ -1252,6 +1252,8 @@ public interface Portal {
 
 	public boolean isCustomPortletMode(PortletMode portletMode);
 
+	public boolean isForwarded(HttpServletRequest httpServletRequest);
+
 	public boolean isForwardedSecure(HttpServletRequest httpServletRequest);
 
 	public boolean isGroupAdmin(User user, long groupId) throws Exception;
