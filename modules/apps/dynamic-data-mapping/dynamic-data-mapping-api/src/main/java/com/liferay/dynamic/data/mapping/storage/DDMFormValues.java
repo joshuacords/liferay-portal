ca6942fd80bbab801a83cc35cfb77910d3c6f47e
Lino Alves <lino.alves@liferay.com>
2020-10-16 14:57:13 -0300

    LPS-98077 partial backport of LPS-115196 : Add new method : 66838196d6f0b90dbf52dde77846391148d5a08b

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/storage/DDMFormValues.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/storage/DDMFormValues.java
@@ -85,6 +85,12 @@ public class DDMFormValues implements Serializable {
 	}
 
 	public Map<String, List<DDMFormFieldValue>> getDDMFormFieldValuesMap() {
+		return getDDMFormFieldValuesMap(false);
+	}
+
+	public Map<String, List<DDMFormFieldValue>> getDDMFormFieldValuesMap(
+			boolean includeNestedDDMFormFieldValues) {
+
 		Map<String, List<DDMFormFieldValue>> ddmFormFieldValuesMap =
 			new LinkedHashMap<>();
 
@@ -100,6 +106,11 @@ public class DDMFormValues implements Serializable {
 			}
 
 			ddmFormFieldValues.add(ddmFormFieldValue);
+			
+			if (includeNestedDDMFormFieldValues) {
+				ddmFormFieldValuesMap.putAll(
+					ddmFormFieldValue.getNestedDDMFormFieldValuesMap());
+			}
 		}
 
 		return ddmFormFieldValuesMap;
