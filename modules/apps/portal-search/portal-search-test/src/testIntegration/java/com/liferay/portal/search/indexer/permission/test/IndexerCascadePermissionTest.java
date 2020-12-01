b7e3c1d6506acda79de71cd7de0db8bc8618ec57
Joshua Cords <joshua.cords@liferay.com>
2020-12-01 09:36:18 -0800

    portal-search-test: guestUser working

--- modules/apps/portal-search/portal-search-test/src/testIntegration/java/com/liferay/portal/search/indexer/permission/test/IndexerCascadePermissionTest.java
+++ modules/apps/portal-search/portal-search-test/src/testIntegration/java/com/liferay/portal/search/indexer/permission/test/IndexerCascadePermissionTest.java
@@ -15,31 +15,36 @@
 package com.liferay.portal.search.indexer.permission.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
-import com.liferay.document.library.kernel.model.DLFileEntry;
 import com.liferay.document.library.kernel.model.DLFolder;
 import com.liferay.document.library.kernel.model.DLFolderConstants;
 import com.liferay.document.library.kernel.service.DLAppLocalService;
 import com.liferay.document.library.kernel.service.DLFileEntryLocalService;
 import com.liferay.document.library.kernel.service.DLFolderLocalService;
+import com.liferay.document.library.kernel.service.DLFolderLocalServiceUtil;
 import com.liferay.document.library.test.util.search.DLFolderSearchFixture;
-import com.liferay.journal.model.JournalArticle;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.ResourceConstants;
+import com.liferay.portal.kernel.model.Role;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.model.role.RoleConstants;
 import com.liferay.portal.kernel.search.Hits;
 import com.liferay.portal.kernel.search.Indexer;
-import com.liferay.portal.kernel.search.IndexerPostProcessor;
 import com.liferay.portal.kernel.search.IndexerRegistry;
-import com.liferay.portal.kernel.search.QueryConfig;
 import com.liferay.portal.kernel.search.SearchContext;
 import com.liferay.portal.kernel.search.SearchException;
 import com.liferay.portal.kernel.search.facet.faceted.searcher.FacetedSearcher;
 import com.liferay.portal.kernel.search.facet.faceted.searcher.FacetedSearcherManager;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.security.permission.PermissionCheckerFactoryUtil;
 import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
+import com.liferay.portal.kernel.service.ResourcePermissionLocalServiceUtil;
+import com.liferay.portal.kernel.service.RoleLocalServiceUtil;
 import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.UserLocalServiceUtil;
 import com.liferay.portal.kernel.test.randomizerbumpers.NumericStringRandomizerBumper;
 import com.liferay.portal.kernel.test.randomizerbumpers.UniqueStringRandomizerBumper;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
@@ -51,7 +56,6 @@ import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
-import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.users.admin.test.util.search.UserSearchFixture;
@@ -62,10 +66,8 @@ import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.osgi.service.component.annotations.Reference;
 
 import java.util.List;
-import java.util.Locale;
 
 /**
  * @author Joshua Cords
@@ -96,12 +98,25 @@ public class IndexerCascadePermissionTest {
 
 		_group = GroupTestUtil.addGroup();
 
+		//Need to be able to set a role on the user
+		_guestRole = RoleLocalServiceUtil.getRole(
+			_group.getCompanyId(), RoleConstants.GUEST);
+
+		_guestUser = getUser(RoleConstants.GUEST);
+
+
+//		UserLocalServiceUtil.setRoleUsers(
+//			_guestRole.getRoleId(), new long[] {_guestUser.getUserId()});
+
 		setUpDLFolderSearchFixture();
 
 		_indexer = indexerRegistry.getIndexer(DLFolder.class);
 
+//		PermissionChecker permissionChecker =
+//			PermissionCheckerFactoryUtil.create(TestPropsValues.getUser());
+
 		PermissionChecker permissionChecker =
-			PermissionCheckerFactoryUtil.create(TestPropsValues.getUser());
+			PermissionCheckerFactoryUtil.create(_guestUser);
 
 		PermissionThreadLocal.setPermissionChecker(permissionChecker);
 
@@ -117,7 +132,7 @@ public class IndexerCascadePermissionTest {
 
 		//_groups = _userSearchFixture.getGroups();
 
-		//_user = _userSearchFixture.addUser(
+		//_guestUser = _userSearchFixture.addUser(
 		//	RandomTestUtil.randomString(), _group);
 
 		//_users = _userSearchFixture.getUsers();
@@ -130,57 +145,59 @@ public class IndexerCascadePermissionTest {
 
 	private void _createDLFolderTree() throws Exception {
 
-		int originalCount = searchCount();
+		int originalCount = searchCount(_guestUser);
 
 		DLFolder parentFolder = _createDLFolder("permissioned folder", DLFolderConstants.DEFAULT_PARENT_FOLDER_ID);
 		DLFolder childFolder = _createDLFolder("permissioned subfolder", parentFolder.getFolderId());
 
 		Assert.assertEquals(
-			"Expected " + originalCount , originalCount+2, searchCount());
+			"Expected " + originalCount , originalCount+2, searchCount(_user));
+
+		//Need to set permissions on the DLFolder new String[] {ActionKeys.VIEW}
+		_resourcePermissionLocalService.setResourcePermissions(
+			TestPropsValues.getCompanyId(), DLFolder.class.getName(), ResourceConstants.SCOPE_INDIVIDUAL,
+			String.valueOf(parentFolder.getFolderId()), _guestRole.getRoleId(), new String[] {});
+
+		Assert.assertEquals(
+			"Expected " + originalCount , originalCount, searchCount(_guestUser));
+
+		//Need to be able to select a facet, see FolderFacetTest
 
 
 	}
 
-	protected int searchCount() {
-		SearchContext searchContext = getSearchContext(LocaleUtil.US, "permissioned");
+	protected User getUser(String roleName) throws Exception {
+		Role role = RoleLocalServiceUtil.getRole(
+			TestPropsValues.getCompanyId(), roleName);
+
+		List<User> users = UserLocalServiceUtil.getRoleUsers(
+			role.getRoleId(), 0, 1);
+
+		return users.get(0);
+	}
+
+	protected int searchCount(User user) {
+		SearchContext searchContext = getSearchContext( user, "permissioned");
 		Hits hits = search(searchContext);
 
 		return hits.getLength();
 	}
 
-	protected SearchContext getSearchContext(Locale locale, String searchTerm) {
-		SearchContext searchContext = getSearchContext(locale);
+	protected SearchContext getSearchContext(User user, String searchTerm) {
+		SearchContext searchContext = getSearchContext(user);
 
 		searchContext.setKeywords(searchTerm);
 
 		return searchContext;
 	}
 
-	protected SearchContext getSearchContext(Locale locale) {
-		SearchContext searchContext = getSearchContext();
-
-		searchContext.setEntryClassNames(
-			new String[] {
-				DLFileEntry.class.getName(), DLFolder.class.getName(), JournalArticle.class.getName()
-			});
-
-		searchContext.setLocale(locale);
-
-		QueryConfig queryConfig = searchContext.getQueryConfig();
-
-		queryConfig.setLocale(locale);
-		queryConfig.setSelectedFieldNames(StringPool.STAR);
-
-		return searchContext;
-	}
-
-	protected SearchContext getSearchContext() {
+	protected SearchContext getSearchContext(User user) {
 		SearchContext searchContext = new SearchContext();
 
 		try {
 			searchContext.setCompanyId(TestPropsValues.getCompanyId());
 			searchContext.setGroupIds(new long[] {_group.getGroupId()});
-			searchContext.setUserId(TestPropsValues.getUserId());
+			searchContext.setUserId(user.getUserId());
 		}
 		catch (PortalException portalException) {
 			throw new RuntimeException(portalException);
@@ -245,8 +262,18 @@ public class IndexerCascadePermissionTest {
 	@Inject
 	protected static FacetedSearcherManager facetedSearcherManager;
 
+	@Inject
+	private ResourcePermissionLocalService _resourcePermissionLocalService;
+
 	@DeleteAfterTestRun
 	private Group _group;
+
+	//@DeleteAfterTestRun
+	private Role _guestRole;
+
+	//@DeleteAfterTestRun
+	private User _guestUser;
+
 
 	private User _user;
 
