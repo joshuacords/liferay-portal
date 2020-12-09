5612df2576c2eddc5a42ee01d4aef99ac202fe80
Kyle Miho <kyle.miho@liferay.com>
2020-12-09 11:50:46 -0800

    LPS-124582 Wait for all Lifecycle Events to finish otherwise Setup Wizard may try to add sample data before the base Regions and Countries are initialized

--- portal-impl/src/com/liferay/portal/internal/servlet/MainServlet.java
+++ portal-impl/src/com/liferay/portal/internal/servlet/MainServlet.java
@@ -420,18 +420,6 @@ public class MainServlet extends HttpServlet {
 			_log.error(exception, exception);
 		}
 
-		if (StartupHelperUtil.isDBNew() &&
-			PropsValues.SETUP_WIZARD_ADD_SAMPLE_DATA) {
-
-			try {
-				SetupWizardSampleDataUtil.addSampleData(
-					PortalInstances.getDefaultCompanyId());
-			}
-			catch (Exception exception) {
-				_log.error(exception, exception);
-			}
-		}
-
 		if (_log.isDebugEnabled()) {
 			_log.debug("Initialize plugins");
 		}
@@ -450,6 +438,18 @@ public class MainServlet extends HttpServlet {
 		StartupHelperUtil.setStartupFinished(true);
 
 		_registerPortalInitialized();
+
+		if (StartupHelperUtil.isDBNew() &&
+			PropsValues.SETUP_WIZARD_ADD_SAMPLE_DATA) {
+
+			try {
+				SetupWizardSampleDataUtil.addSampleData(
+					PortalInstances.getDefaultCompanyId());
+			}
+			catch (Exception exception) {
+				_log.error(exception, exception);
+			}
+		}
 
 		ThreadLocalCacheManager.clearAll(Lifecycle.REQUEST);
 	}
