c43154ad68df0df680293928247293cce08d7a4d
Brian Chan <brian.chan@liferay.com>
2021-02-05 09:03:54 -0300

    LPS-122906 SF

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/upgrade/v3_2_9/UpgradeDDMFormInstance.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-service/src/main/java/com/liferay/dynamic/data/mapping/internal/upgrade/v3_2_9/UpgradeDDMFormInstance.java
@@ -81,7 +81,6 @@ public class UpgradeDDMFormInstance extends UpgradeProcess {
 				if (_upgradeSettings(settingsJSONObject)) {
 					updatePreparedStatement.setString(
 						1, settingsJSONObject.toString());
-
 					updatePreparedStatement.setLong(
 						2, rs.getLong(idColumnName));
 
