09f4be02ff7787a1b757fcc4902ff8cf83c02b73
Cristina Rodriguez <cristina.rodriguez@liferay.com>
2021-02-09 16:20:50 +0100

    LPS-127795 Partial backport of LPS-118102 Original commit message: LPS-118102 Move rule validation to a new class 149afa4f01bdc1b36b8b54ee8323bb370b073f8e

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-validator/src/main/java/com/liferay/dynamic/data/mapping/validator/internal/DDMFormValidatorImpl.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-validator/src/main/java/com/liferay/dynamic/data/mapping/validator/internal/DDMFormValidatorImpl.java
@@ -24,7 +24,6 @@ import com.liferay.dynamic.data.mapping.model.DDMFormFieldOptions;
 import com.liferay.dynamic.data.mapping.model.DDMFormFieldType;
 import com.liferay.dynamic.data.mapping.model.DDMFormFieldValidation;
 import com.liferay.dynamic.data.mapping.model.DDMFormFieldValidationExpression;
-import com.liferay.dynamic.data.mapping.model.DDMFormRule;
 import com.liferay.dynamic.data.mapping.model.LocalizedValue;
 import com.liferay.dynamic.data.mapping.validator.DDMFormValidationException;
 import com.liferay.dynamic.data.mapping.validator.DDMFormValidationException.MustNotDuplicateFieldName;
@@ -38,11 +37,11 @@ import com.liferay.dynamic.data.mapping.validator.DDMFormValidationException.Mus
 import com.liferay.dynamic.data.mapping.validator.DDMFormValidationException.MustSetValidCharactersForFieldName;
 import com.liferay.dynamic.data.mapping.validator.DDMFormValidationException.MustSetValidCharactersForFieldType;
 import com.liferay.dynamic.data.mapping.validator.DDMFormValidationException.MustSetValidDefaultLocaleForProperty;
-import com.liferay.dynamic.data.mapping.validator.DDMFormValidationException.MustSetValidFormRuleExpression;
 import com.liferay.dynamic.data.mapping.validator.DDMFormValidationException.MustSetValidIndexType;
 import com.liferay.dynamic.data.mapping.validator.DDMFormValidationException.MustSetValidValidationExpression;
 import com.liferay.dynamic.data.mapping.validator.DDMFormValidationException.MustSetValidVisibilityExpression;
 import com.liferay.dynamic.data.mapping.validator.DDMFormValidator;
+import com.liferay.dynamic.data.mapping.validator.internal.util.DDMFormRuleValidatorUtil;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.bean.BeanPropertiesUtil;
 import com.liferay.portal.kernel.util.ArrayUtil;
@@ -74,6 +73,9 @@ public class DDMFormValidatorImpl implements DDMFormValidator {
 		throws DDMFormFieldValueValidationException,
 			   DDMFormValidationException {
 
+		DDMFormRuleValidatorUtil.validateDDMFormRules(
+			_ddmExpressionFactory, ddmForm.getDDMFormRules());
+
 		validateDDMFormLocales(ddmForm);
 
 		List<DDMFormField> ddmFormFields = ddmForm.getDDMFormFields();
@@ -85,8 +87,6 @@ public class DDMFormValidatorImpl implements DDMFormValidator {
 		validateDDMFormFields(
 			ddmFormFields, new HashSet<String>(), ddmForm.getAvailableLocales(),
 			ddmForm.getDefaultLocale());
-
-		validateDDMFormRules(ddmForm.getDDMFormRules());
 	}
 
 	@Reference(unbind = "-")
@@ -94,26 +94,6 @@ public class DDMFormValidatorImpl implements DDMFormValidator {
 		DDMExpressionFactory ddmExpressionFactory) {
 
 		_ddmExpressionFactory = ddmExpressionFactory;
-	}
-
-	protected void validateDDMExpression(
-			String expressionType, String ddmExpressionString)
-		throws DDMFormValidationException {
-
-		if (Validator.isNull(ddmExpressionString)) {
-			throw new MustSetValidFormRuleExpression(
-				expressionType, ddmExpressionString,
-				new DDMExpressionException());
-		}
-
-		try {
-			_ddmExpressionFactory.createBooleanDDMExpression(
-				ddmExpressionString);
-		}
-		catch (DDMExpressionException ddmExpressionException) {
-			throw new MustSetValidFormRuleExpression(
-				expressionType, ddmExpressionString, ddmExpressionException);
-		}
 	}
 
 	protected void validateDDMFormAvailableLocales(
@@ -361,24 +341,6 @@ public class DDMFormValidatorImpl implements DDMFormValidator {
 
 		validateDDMFormAvailableLocales(
 			ddmForm.getAvailableLocales(), defaultLocale);
-	}
-
-	protected void validateDDMFormRule(DDMFormRule ddmFormRule)
-		throws DDMFormValidationException {
-
-		for (String action : ddmFormRule.getActions()) {
-			validateDDMExpression("action", action);
-		}
-
-		validateDDMExpression("condition", ddmFormRule.getCondition());
-	}
-
-	protected void validateDDMFormRules(List<DDMFormRule> ddmFormRules)
-		throws DDMFormValidationException {
-
-		for (DDMFormRule ddmFormRule : ddmFormRules) {
-			validateDDMFormRule(ddmFormRule);
-		}
 	}
 
 	protected void validateOptionalDDMFormFieldLocalizedProperty(
