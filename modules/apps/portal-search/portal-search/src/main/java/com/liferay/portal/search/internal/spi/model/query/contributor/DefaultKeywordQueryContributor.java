3d735b486b3d24959ee967162371f50b940b5c7e
Joshua Cords <joshua.cords@liferay.com>
2021-03-25 13:55:47 -0700

    LPS-129113 portal-search: implement keyword trimming
    https://issues.liferay.com/browse/LPS-129113

--- modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/spi/model/query/contributor/DefaultKeywordQueryContributor.java
+++ modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/spi/model/query/contributor/DefaultKeywordQueryContributor.java
@@ -14,19 +14,23 @@
 
 package com.liferay.portal.search.internal.spi.model.query.contributor;
 
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.search.BooleanQuery;
 import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.SetUtil;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.search.configuration.DefaultKeywordQueryConfiguration;
 import com.liferay.portal.search.query.QueryHelper;
 import com.liferay.portal.search.spi.model.query.contributor.KeywordQueryContributor;
 import com.liferay.portal.search.spi.model.query.contributor.helper.KeywordQueryContributorHelper;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -40,7 +44,10 @@ import org.osgi.service.component.annotations.Reference;
  */
 @Component(
 	configurationPid = "com.liferay.portal.search.configuration.DefaultKeywordQueryConfiguration",
-	immediate = true, service = KeywordQueryContributor.class
+	immediate = true,
+	service = {
+		DefaultKeywordQueryContributor.class, KeywordQueryContributor.class
+	}
 )
 public class DefaultKeywordQueryContributor implements KeywordQueryContributor {
 
@@ -76,12 +83,46 @@ public class DefaultKeywordQueryContributor implements KeywordQueryContributor {
 		}
 	}
 
+	public int getMaximumKeywordSize() {
+		return _maximumKeywordSize;
+	}
+
+	public String trimKeywords(String keywords) {
+		if (keywords.length() < _maximumKeywordSize) {
+			return keywords;
+		}
+
+		List<String> keywordList = ListUtil.fromString(
+			keywords, StringPool.SPACE);
+
+		for (int i = 0; i < keywordList.size(); i++) {
+			String keyword = keywordList.get(i);
+
+			if (keyword.length() > _maximumKeywordSize) {
+				keywordList.set(
+					i,
+					StringUtil.shorten(
+						keyword, _maximumKeywordSize, StringPool.BLANK));
+			}
+		}
+
+		return ListUtil.toString(
+			keywordList, StringPool.NULL, StringPool.SPACE);
+	}
+
 	@Activate
 	@Modified
 	protected void activate(Map<String, Object> properties) {
 		DefaultKeywordQueryConfiguration defaultKeywordQueryConfiguration =
 			ConfigurableUtil.createConfigurable(
 				DefaultKeywordQueryConfiguration.class, properties);
+
+		_maximumKeywordSize =
+			defaultKeywordQueryConfiguration.maximumKeywordSize();
+
+		if (_maximumKeywordSize <= 0) {
+			_maximumKeywordSize = 100;
+		}
 
 		_disabledEntryClassNames = SetUtil.fromArray(
 			defaultKeywordQueryConfiguration.disabledEntryClassNames());
@@ -94,5 +135,6 @@ public class DefaultKeywordQueryContributor implements KeywordQueryContributor {
 		DefaultKeywordQueryContributor.class);
 
 	private volatile Set<String> _disabledEntryClassNames;
+	private volatile int _maximumKeywordSize;
 
 }
