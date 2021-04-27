ac6edc09c2330e173266658cf4cc5a7baac4c3c8
Adolfo Pérez Álvarez <adolfo.perez@liferay.com>
2021-04-27 16:53:27 +0200

    LPS-130384 We don't need the "path" field, "title" is enough

--- modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/search/spi/model/query/contributor/DLFileEntryKeywordQueryContributor.java
+++ modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/search/spi/model/query/contributor/DLFileEntryKeywordQueryContributor.java
@@ -48,8 +48,6 @@ public class DLFileEntryKeywordQueryContributor
 			queryHelper.addSearchTerm(
 				booleanQuery, searchContext, Field.DESCRIPTION, false);
 			queryHelper.addSearchTerm(
-				booleanQuery, searchContext, Field.TITLE, false);
-			queryHelper.addSearchTerm(
 				booleanQuery, searchContext, Field.USER_NAME, false);
 		}
 
@@ -59,9 +57,11 @@ public class DLFileEntryKeywordQueryContributor
 			booleanQuery, searchContext, "extension", false);
 		queryHelper.addSearchTerm(
 			booleanQuery, searchContext, "fileEntryTypeId", false);
-		queryHelper.addSearchTerm(booleanQuery, searchContext, "path", false);
 		queryHelper.addSearchLocalizedTerm(
 			booleanQuery, searchContext, Field.CONTENT, false);
+		queryHelper.addSearchTerm(
+			booleanQuery, searchContext, Field.TITLE, false);
+
 	}
 
 	@Reference
