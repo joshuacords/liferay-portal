ba3bac3edd501f73a863217e84079323d48fc68c
Bryan Engler <bryan.engler@liferay.com>
2021-04-28 13:08:02 -0700

    LPS-131038 portal-search-test: Add integration test
    https://issues.liferay.com/browse/LPS-131038

---
+++ modules/apps/portal-search/portal-search-test/src/testIntegration/java/com/liferay/portal/search/searcher/test/SearchResponseGetResponseStringTest.java
@@ -0,0 +1,108 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.searcher.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.search.constants.SearchContextAttributes;
+import com.liferay.portal.search.query.Queries;
+import com.liferay.portal.search.searcher.SearchRequestBuilderFactory;
+import com.liferay.portal.search.searcher.SearchResponse;
+import com.liferay.portal.search.searcher.Searcher;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Bryan Engler
+ */
+@RunWith(Arquillian.class)
+public class SearchResponseGetResponseStringTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
+
+	@Before
+	public void setUp() throws Exception {
+		_companyId = TestPropsValues.getCompanyId();
+	}
+
+	@Test
+	public void testGetResponseStringContainsResponse() {
+		SearchResponse searchResponse = _searcher.search(
+			_searchRequestBuilderFactory.builder(
+			).companyId(
+				_companyId
+			).includeResponseString(
+				true
+			).query(
+				_queries.string("test")
+			).withSearchContext(
+				searchContext -> searchContext.setAttribute(
+					SearchContextAttributes.ATTRIBUTE_KEY_EMPTY_SEARCH,
+					true)
+			).build());
+
+		Assert.assertNotNull(searchResponse.getResponseString());
+
+		Assert.assertNotEquals(
+			StringPool.BLANK, searchResponse.getResponseString());
+	}
+
+	@Test
+	public void testGetResponseStringContainsException() {
+		SearchResponse searchResponse = _searcher.search(
+			_searchRequestBuilderFactory.builder(
+			).companyId(
+				_companyId
+			).query(
+				_queries.string("t/est")
+			).withSearchContext(
+				searchContext -> searchContext.setAttribute(
+					SearchContextAttributes.ATTRIBUTE_KEY_EMPTY_SEARCH,
+					true)
+			).build());
+
+		Assert.assertNotNull(searchResponse.getResponseString());
+
+		Assert.assertNotEquals(
+			StringPool.BLANK, searchResponse.getResponseString());
+	}
+
+	private long _companyId;
+
+	@Inject
+	private Queries _queries;
+
+	@Inject
+	private Searcher _searcher;
+
+	@Inject
+	private SearchRequestBuilderFactory _searchRequestBuilderFactory;
+
+}
