3c19f0be70ab9074952337f5588e58ddeb1e54fb
Adolfo Pérez Álvarez <adolfo.perez@liferay.com>
2021-05-17 11:14:47 +0200

    LPS-130384 SF

--- modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/search/spi/model/index/contributor/DLFileEntryModelDocumentContributor.java
+++ modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/search/spi/model/index/contributor/DLFileEntryModelDocumentContributor.java
@@ -25,7 +25,6 @@ import com.liferay.dynamic.data.mapping.kernel.StorageEngineManager;
 import com.liferay.petra.string.CharPool;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
-import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
@@ -106,7 +105,7 @@ public class DLFileEntryModelDocumentContributor
 			if (indexContent) {
 				if (is != null) {
 					try {
-						Locale defaultLocale = portal.getSiteDefaultLocale(
+						Locale defaultLocale = _portal.getSiteDefaultLocale(
 							dlFileEntry.getGroupId());
 
 						String localizedField = Field.getLocalizedName(
@@ -139,7 +138,7 @@ public class DLFileEntryModelDocumentContributor
 			String title = dlFileEntry.getTitle();
 
 			if (dlFileEntry.isInTrash()) {
-				title = trashHelper.getOriginalTitle(title);
+				title = _trashHelper.getOriginalTitle(title);
 			}
 
 			document.addText(Field.TITLE, title);
@@ -155,7 +154,7 @@ public class DLFileEntryModelDocumentContributor
 
 			document.addText(
 				"ddmContent",
-				extractDDMContent(dlFileVersion, LocaleUtil.getSiteDefault()));
+				_extractDDMContent(dlFileVersion, LocaleUtil.getSiteDefault()));
 			document.addKeyword("extension", dlFileEntry.getExtension());
 			document.addKeyword(
 				"fileEntryTypeId", dlFileEntry.getFileEntryTypeId());
@@ -169,11 +168,11 @@ public class DLFileEntryModelDocumentContributor
 			document.addKeyword("readCount", dlFileEntry.getReadCount());
 			document.addNumber("size", dlFileEntry.getSize());
 
-			addFileEntryTypeAttributes(document, dlFileVersion);
+			_addFileEntryTypeAttributes(document, dlFileVersion);
 
 			if (dlFileEntry.isInHiddenFolder()) {
 				List<RelatedEntryIndexer> relatedEntryIndexers =
-					relatedEntryIndexerRegistry.getRelatedEntryIndexers(
+					_relatedEntryIndexerRegistry.getRelatedEntryIndexers(
 						dlFileEntry.getClassName());
 
 				if (ListUtil.isNotEmpty(relatedEntryIndexers)) {
@@ -187,7 +186,7 @@ public class DLFileEntryModelDocumentContributor
 							document);
 
 						documentHelper.setAttachmentOwnerKey(
-							portal.getClassNameId(dlFileEntry.getClassName()),
+							_portal.getClassNameId(dlFileEntry.getClassName()),
 							dlFileEntry.getClassPK());
 
 						document.addKeyword(Field.RELATED_ENTRY, true);
@@ -213,22 +212,21 @@ public class DLFileEntryModelDocumentContributor
 		}
 	}
 
-	protected void addFileEntryTypeAttributes(
-			Document document, DLFileVersion dlFileVersion)
-		throws PortalException {
+	private void _addFileEntryTypeAttributes(
+		Document document, DLFileVersion dlFileVersion) {
 
 		List<DLFileEntryMetadata> dlFileEntryMetadatas =
-			dlFileEntryMetadataLocalService.getFileVersionFileEntryMetadatas(
+			_dlFileEntryMetadataLocalService.getFileVersionFileEntryMetadatas(
 				dlFileVersion.getFileVersionId());
 
 		for (DLFileEntryMetadata dlFileEntryMetadata : dlFileEntryMetadatas) {
 			try {
 				DDMFormValues ddmFormValues =
-					storageEngineManager.getDDMFormValues(
+					_storageEngineManager.getDDMFormValues(
 						dlFileEntryMetadata.getDDMStorageId());
 
 				if (ddmFormValues != null) {
-					ddmStructureManager.addAttributes(
+					_ddmStructureManager.addAttributes(
 						dlFileEntryMetadata.getDDMStructureId(), document,
 						ddmFormValues);
 				}
@@ -241,12 +239,11 @@ public class DLFileEntryModelDocumentContributor
 		}
 	}
 
-	protected String extractDDMContent(
-			DLFileVersion dlFileVersion, Locale locale)
-		throws Exception {
+	private String _extractDDMContent(
+		DLFileVersion dlFileVersion, Locale locale) {
 
 		List<DLFileEntryMetadata> dlFileEntryMetadatas =
-			dlFileEntryMetadataLocalService.getFileVersionFileEntryMetadatas(
+			_dlFileEntryMetadataLocalService.getFileVersionFileEntryMetadatas(
 				dlFileVersion.getFileVersionId());
 
 		StringBundler sb = new StringBundler(dlFileEntryMetadatas.size() * 2);
@@ -254,12 +251,12 @@ public class DLFileEntryModelDocumentContributor
 		for (DLFileEntryMetadata dlFileEntryMetadata : dlFileEntryMetadatas) {
 			try {
 				DDMFormValues ddmFormValues =
-					storageEngineManager.getDDMFormValues(
+					_storageEngineManager.getDDMFormValues(
 						dlFileEntryMetadata.getDDMStorageId());
 
 				if (ddmFormValues != null) {
 					sb.append(
-						ddmStructureManager.extractAttributes(
+						_ddmStructureManager.extractAttributes(
 							dlFileEntryMetadata.getDDMStructureId(),
 							ddmFormValues, locale));
 
@@ -281,22 +278,22 @@ public class DLFileEntryModelDocumentContributor
 	}
 
 	@Reference
-	protected DDMStructureManager ddmStructureManager;
+	private DDMStructureManager _ddmStructureManager;
 
 	@Reference
-	protected DLFileEntryMetadataLocalService dlFileEntryMetadataLocalService;
+	private DLFileEntryMetadataLocalService _dlFileEntryMetadataLocalService;
 
 	@Reference
-	protected Portal portal;
+	private Portal _portal;
 
 	@Reference
-	protected RelatedEntryIndexerRegistry relatedEntryIndexerRegistry;
+	private RelatedEntryIndexerRegistry _relatedEntryIndexerRegistry;
 
 	@Reference
-	protected StorageEngineManager storageEngineManager;
+	private StorageEngineManager _storageEngineManager;
 
 	@Reference
-	protected TrashHelper trashHelper;
+	private TrashHelper _trashHelper;
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		DLFileEntryModelDocumentContributor.class);
