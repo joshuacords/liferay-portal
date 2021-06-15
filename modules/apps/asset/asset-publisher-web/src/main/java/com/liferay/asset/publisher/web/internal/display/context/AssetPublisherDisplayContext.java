1c2bc697727e8b01899c88b727c05ac67a66015f
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2021-06-15 10:25:33 +0200

    LPS-134062 Use site default locale instead user locale to build filter structure field name

--- modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/internal/display/context/AssetPublisherDisplayContext.java
+++ modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/internal/display/context/AssetPublisherDisplayContext.java
@@ -410,7 +410,7 @@ public class AssetPublisherDisplayContext {
 		_assetEntryQuery.setEnablePermissions(isEnablePermissions());
 
 		configureSubtypeFieldFilter(
-			_assetEntryQuery, _themeDisplay.getLocale());
+			_assetEntryQuery, _themeDisplay.getSiteDefaultLocale());
 
 		_assetEntryQuery.setPaginationType(getPaginationType());
 
