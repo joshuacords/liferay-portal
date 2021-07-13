1153bb6ded106ba873d6cbd3a6d587589640e7c0
Joshua Cords <joshua.cords@liferay.com>
2021-07-13 10:55:36 -0700

    LPS-135568 journal-service: JournalArticle and JournalFolder Implement BaseChildModel
    https://issues.liferay.com/browse/LPS-135568

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/model/impl/JournalArticleModelImpl.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/model/impl/JournalArticleModelImpl.java
@@ -369,6 +369,16 @@ public class JournalArticleModelImpl
 	}
 
 	@Override
+	public String getParentClassName() {
+		return "com.liferay.journal.model.JournalFolder";
+	}
+
+	@Override
+	public String getParentClassPK() {
+		return String.valueOf(getFolderId());
+	}
+
+	@Override
 	public long getPrimaryKey() {
 		return _id;
 	}
