0e291e945f1e0af98c166bfe967f3fe5d6c36456
Renato Rêgo <renato.rego@liferay.com>
2021-08-12 09:17:57 -0300

    LPS-30938 Initialize Test Helpers using userId

--- modules/apps/dynamic-data-lists/dynamic-data-lists-test/src/testIntegration/java/com/liferay/dynamic/data/lists/search/test/DDLRecordSearchTest.java
+++ modules/apps/dynamic-data-lists/dynamic-data-lists-test/src/testIntegration/java/com/liferay/dynamic/data/lists/search/test/DDLRecordSearchTest.java
@@ -86,7 +86,8 @@ public class DDLRecordSearchTest {
 
 		DDLRecordSet recordSet = addRecordSet();
 
-		_recordTestHelper = new DDLRecordTestHelper(_group, recordSet);
+		_recordTestHelper = new DDLRecordTestHelper(
+			_user.getUserId(), _group, recordSet);
 		_searchContext = getSearchContext(_group, _user, recordSet);
 	}
 
@@ -192,16 +193,17 @@ public class DDLRecordSearchTest {
 
 		User user = UserLocalServiceUtil.getDefaultUser(companyId);
 
+		long userId = user.getUserId();
+
 		Group group = GroupTestUtil.addGroup(
-			companyId, user.getUserId(),
-			GroupConstants.DEFAULT_PARENT_GROUP_ID);
+			companyId, userId, GroupConstants.DEFAULT_PARENT_GROUP_ID);
 
 		DDLRecordSetTestHelper recordSetTestHelper = new DDLRecordSetTestHelper(
-			group);
+			userId, group);
 
 		DDMStructureTestHelper ddmStructureTestHelper =
 			new DDMStructureTestHelper(
-				PortalUtil.getClassNameId(DDLRecordSet.class), group);
+				userId, PortalUtil.getClassNameId(DDLRecordSet.class), group);
 
 		Set<Locale> locales = DDMFormTestUtil.createAvailableLocales(
 			new Locale[] {LocaleUtil.US, LocaleUtil.JAPAN});
@@ -246,7 +248,7 @@ public class DDLRecordSearchTest {
 			ddmFormValues, WorkflowConstants.ACTION_PUBLISH);
 
 		DDLRecordTestHelper recordTestHelper = new DDLRecordTestHelper(
-			group, recordSet);
+			userId, group, recordSet);
 
 		recordTestHelper.addRecord(
 			ddmFormValues, WorkflowConstants.ACTION_PUBLISH);
@@ -413,10 +415,11 @@ public class DDLRecordSearchTest {
 
 	protected DDLRecordSet addRecordSet() throws Exception {
 		DDLRecordSetTestHelper recordSetTestHelper = new DDLRecordSetTestHelper(
-			_group);
+			_user.getUserId(), _group);
 
 		DDMStructureTestHelper ddmStructureTestHelper =
 			new DDMStructureTestHelper(
+				_user.getUserId(),
 				PortalUtil.getClassNameId(DDLRecordSet.class), _group);
 
 		DDMStructure ddmStructure = ddmStructureTestHelper.addStructure(
