259254a4af9ba2c9fc3952b8e0392beed28c257f
Renato Rêgo <renato.rego@liferay.com>
2021-08-12 10:16:43 -0300

    LPS-30938 Update search tests which won't find the searched resources due to the lack of VIEW permission

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-test/src/testIntegration/java/com/liferay/dynamic/data/mapping/search/test/DDMFormInstanceRecordSearchTest.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-test/src/testIntegration/java/com/liferay/dynamic/data/mapping/search/test/DDMFormInstanceRecordSearchTest.java
@@ -108,7 +108,7 @@ public class DDMFormInstanceRecordSearchTest {
 		_searchContext.setKeywords("Simple description");
 		_searchContext.setUserId(user.getUserId());
 
-		assertSearch("description", 1);
+		assertSearch("description", 0);
 	}
 
 	@Test
