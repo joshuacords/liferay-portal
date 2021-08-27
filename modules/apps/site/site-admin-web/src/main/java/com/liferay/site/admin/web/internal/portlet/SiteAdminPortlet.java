47a7e9d07f6c88138dc979093e7d7c4f04cb62e7
tototrinh <to.trinh@codeenginestudio.com>
2021-08-27 10:56:16 +0700

    LPS-137334 Add integration test

--- modules/apps/site/site-admin-web/src/main/java/com/liferay/site/admin/web/internal/portlet/SiteAdminPortlet.java
+++ modules/apps/site/site-admin-web/src/main/java/com/liferay/site/admin/web/internal/portlet/SiteAdminPortlet.java
@@ -1030,9 +1030,7 @@ public class SiteAdminPortlet extends MVCPortlet {
 			creationType.equals(
 				SiteAdminConstants.CREATION_TYPE_SITE_TEMPLATE)) {
 
-			ActionUtil.updateLayoutSetPrototypesLinks(actionRequest, liveGroup);
-
-			ActionUtil.updateWorkflowDefinitionLinks(actionRequest, liveGroup);
+			updateGroupFromSiteTemplate(actionRequest, liveGroup);
 		}
 		else if (creationType.equals(
 					SiteAdminConstants.CREATION_TYPE_INITIALIZER)) {
@@ -1056,6 +1054,15 @@ public class SiteAdminPortlet extends MVCPortlet {
 		themeDisplay.setSiteGroupId(liveGroup.getGroupId());
 
 		return liveGroup;
+	}
+
+	protected void updateGroupFromSiteTemplate(
+			ActionRequest actionRequest, Group group)
+		throws Exception {
+
+		ActionUtil.updateLayoutSetPrototypesLinks(actionRequest, group);
+
+		ActionUtil.updateWorkflowDefinitionLinks(actionRequest, group);
 	}
 
 	protected void validateDefaultLocaleGroupName(
