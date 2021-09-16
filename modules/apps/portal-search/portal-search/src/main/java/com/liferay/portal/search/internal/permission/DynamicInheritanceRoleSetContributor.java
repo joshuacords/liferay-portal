31e57fdf65c49c4ecdce1c67d911d58e398ce10e
Joshua Cords <joshua.cords@liferay.com>
2021-09-16 10:15:26 -0700

    LPS-134986 portal-search: add DynamicInheritanceRoleSetContributor

--- modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/permission/DynamicInheritanceRoleSetContributor.java
+++ modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/permission/DynamicInheritanceRoleSetContributor.java
@@ -1,21 +1,63 @@
 package com.liferay.portal.search.internal.permission;
 
 import com.liferay.petra.function.UnsafeFunction;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.GroupedModel;
+import com.liferay.portal.kernel.model.ResourceConstants;
+import com.liferay.portal.kernel.model.ResourcePermission;
+import com.liferay.portal.kernel.model.Role;
+import com.liferay.portal.kernel.model.role.RoleConstants;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
 import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
+import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
+import com.liferay.portal.kernel.service.RoleLocalService;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.search.spi.model.permission.SearchPermissionDefinition;
 import com.liferay.portal.search.spi.model.permission.context.RoleSetContributorContext;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
+
+@Component(service = DynamicInheritanceRoleSetContributor.class)
 public class DynamicInheritanceRoleSetContributor
 	<C extends GroupedModel, P extends GroupedModel> implements
-	SearchPermissionDefinition.RoleSetContributor{
+	SearchPermissionDefinition.RoleSetContributor<C>{
 
 	@Override
-	public void apply(RoleSetContributorContext roleSetContributorContext) {
+	public void apply(
+		RoleSetContributorContext roleSetContributorContext, C child)
+		throws PortalException {
+
+		P parent = _fetchParentUnsafeFunction.apply(child);
+
+		List<Role> baseRoles = _resourcePermissionLocalService.getRoles(
+			roleSetContributorContext.getCompanyId(), child.getModelClassName(),
+			ResourceConstants.SCOPE_INDIVIDUAL,
+			GetterUtil.getString(child.getPrimaryKeyObj()), ActionKeys.VIEW);
+
+		Role guestRole = _roleLocalService.getRole(
+			roleSetContributorContext.getCompanyId(), RoleConstants.GUEST);
+
+		if (parent == null) {
+			if (baseRoles.contains(guestRole)) {
+				_assignGuestRoleIdSet(roleSetContributorContext);
+
+				return;
+			}
+
+			_assignListAsIndividualRoleIdSets(roleSetContributorContext,
+				child.getModelClassName(),
+				GetterUtil.getLong(child.getPrimaryKeyObj()), baseRoles);
+
+			return;
+		}
 
 	}
 
@@ -35,10 +77,94 @@ public class DynamicInheritanceRoleSetContributor
 			parentModelResourcePermission.getPortletResourcePermission());
 	}
 
+	private void _assignGuestRoleIdSet(
+		RoleSetContributorContext roleSetContributorContext)
+		throws PortalException {
+
+		Set<Set<String>> roleIdSets = new HashSet<>();
+		Set<String> roleIds = new HashSet<>();
+
+		Role guestRole = _roleLocalService.getRole(
+			roleSetContributorContext.getCompanyId(), RoleConstants.GUEST);
+
+		roleIds.add(String.valueOf(guestRole.getRoleId()));
+
+		roleIdSets.add(roleIds);
+
+		roleSetContributorContext.setViewPermissionRoleIdSets(roleIdSets);
+	}
+
+	private void _assignListAsIndividualRoleIdSets(
+		RoleSetContributorContext roleSetContributorContext, String className,
+		long classPK, List<Role> roles)
+		throws PortalException {
+
+		Set<Set<String>> roleIdSets = new HashSet<>();
+
+		for (Role role : roles) {
+			Set<String> roleIds = new HashSet<>();
+
+			roleIds.add(
+				_roleToRoleId(
+					roleSetContributorContext.getCompanyId(),
+					roleSetContributorContext.getGroupId(), className, classPK,
+					role));
+
+			roleIdSets.add(roleIds);
+		}
+
+		roleSetContributorContext.setViewPermissionRoleIdSets(
+			roleIdSets);
+	}
+
+	private String _roleToRoleId(
+		long companyId, long groupId, String className, long classPK,
+		Role role)
+		throws PortalException {
+
+		Role ownerRole = _roleLocalService.getRole(
+			companyId, RoleConstants.OWNER);
+
+		if ((role.getType() == RoleConstants.TYPE_ORGANIZATION) ||
+			(role.getType() == RoleConstants.TYPE_SITE)) {
+
+			return groupId + StringPool.DASH + role.getRoleId();
+		}
+		else if (_isOwnerRoleId(companyId, role.getRoleId())) {
+			ResourcePermission resourcePermission =
+				_resourcePermissionLocalService.getResourcePermission(
+					companyId, className, ResourceConstants.SCOPE_INDIVIDUAL,
+					String.valueOf(classPK), ownerRole.getRoleId());
+
+			return resourcePermission.getOwnerId() + StringPool.DASH +
+				   role.getRoleId();
+		}
+		else {
+			return String.valueOf(role.getRoleId());
+		}
+	}
+
+	private boolean _isOwnerRoleId(long companyId, long roleId) {
+		Role ownerRole = _roleLocalService.fetchRole(
+			companyId, RoleConstants.OWNER);
+
+		if ((ownerRole != null) && (roleId == ownerRole.getRoleId())) {
+			return true;
+		}
+
+		return false;
+	}
+
 	private final boolean _checkParentAccess;
 	private final UnsafeFunction<C, P, ? extends PortalException>
 		_fetchParentUnsafeFunction;
 	private final ModelResourcePermission<P> _parentModelResourcePermission;
 	private final PortletResourcePermission _portletResourcePermission;
+
+	@Reference
+	private ResourcePermissionLocalService _resourcePermissionLocalService;
+
+	@Reference
+	private RoleLocalService _roleLocalService;
 
 }
