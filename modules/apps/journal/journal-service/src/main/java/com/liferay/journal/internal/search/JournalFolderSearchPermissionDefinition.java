9ab553d6c84a29a1736daf76ff45641345964207
Joshua Cords <joshua.cords@liferay.com>
2021-10-08 12:37:35 -0700

    LPS-134986 try using the helper as a component - not working in OSGI

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/search/JournalFolderSearchPermissionDefinition.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/search/JournalFolderSearchPermissionDefinition.java
@@ -24,8 +24,9 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
 import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
 import com.liferay.portal.kernel.service.RoleLocalService;
-import com.liferay.portal.search.spi.model.permission.SearchPermissionDefinition;
 import com.liferay.portal.search.spi.model.permission.DynamicInheritanceRoleSetContributor;
+import com.liferay.portal.search.spi.model.permission.RoleSetContributorHelper;
+import com.liferay.portal.search.spi.model.permission.SearchPermissionDefinition;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -56,7 +57,8 @@ public class JournalFolderSearchPermissionDefinition
 			new DynamicInheritanceRoleSetContributor<>(
 				_journalFolderModelResourcePermission,
 				_getFetchParentFunction(), false,
-				_resourcePermissionLocalService, _roleLocalService));
+				_resourcePermissionLocalService, _roleLocalService,
+				_roleSetContributorHelper));
 //			new WorkflowedModelRoleSetContributor());
 	}
 
@@ -92,5 +94,8 @@ public class JournalFolderSearchPermissionDefinition
 
 	@Reference
 	private RoleLocalService _roleLocalService;
+
+	@Reference
+	private RoleSetContributorHelper _roleSetContributorHelper;
 
 }
