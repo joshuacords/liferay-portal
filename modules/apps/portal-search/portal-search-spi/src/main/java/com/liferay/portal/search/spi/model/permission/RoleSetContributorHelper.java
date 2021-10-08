b152a195e260d89a16ed933153cb4cd912ff9148
Joshua Cords <joshua.cords@liferay.com>
2021-10-08 12:36:53 -0700

    LPS-134986 portal-search-spi: extract roleId logic to a helper for all contributors

---
+++ modules/apps/portal-search/portal-search-spi/src/main/java/com/liferay/portal/search/spi/model/permission/RoleSetContributorHelper.java
@@ -0,0 +1,119 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.search.spi.model.permission;
+
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.ResourceConstants;
+import com.liferay.portal.kernel.model.ResourcePermission;
+import com.liferay.portal.kernel.model.Role;
+import com.liferay.portal.kernel.model.role.RoleConstants;
+import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
+import com.liferay.portal.kernel.service.RoleLocalService;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author Joshua Cords
+ */
+@Component(immediate = true, service = {})
+public class RoleSetContributorHelper {
+
+	public void assignRolesAsIndividualAccessRoleIdSets(
+		RoleSetContributorContext roleSetContributorContext,
+		String className, long classPK, List<Role> roles)
+		throws PortalException {
+
+		roleSetContributorContext.addAccessPermissionRoleIdSet(
+			_createRoleIdSet(
+				roleSetContributorContext, className, classPK, roles));
+	}
+
+	public void assignRolesAsIndividualViewRoleIdSets(
+		RoleSetContributorContext roleSetContributorContext,
+		String className, long classPK, List<Role> roles)
+		throws PortalException {
+
+		roleSetContributorContext.addViewPermissionRoleIdSet(
+			_createRoleIdSet(
+				roleSetContributorContext, className, classPK, roles));
+	}
+
+	private Set<String> _createRoleIdSet(
+		RoleSetContributorContext roleSetContributorContext,
+		String className, long classPK, List<Role> roles)
+		throws PortalException {
+
+		Set<String> roleIdSet = new HashSet<>();
+
+		for (Role role : roles) {
+			roleIdSet.add(
+				_roleToRoleId(
+					roleSetContributorContext.getCompanyId(),
+					roleSetContributorContext.getGroupId(), className, classPK,
+					role));
+		}
+
+		return roleIdSet;
+	}
+
+	private String _roleToRoleId(
+		long companyId, long groupId, String className, long classPK,
+		Role role)
+		throws PortalException {
+
+		Role ownerRole = _roleLocalService.getRole(
+			companyId, RoleConstants.OWNER);
+
+		if ((role.getType() == RoleConstants.TYPE_ORGANIZATION) ||
+			(role.getType() == RoleConstants.TYPE_SITE)) {
+
+			return groupId + StringPool.DASH + role.getRoleId();
+		}
+		else if (_isOwnerRoleId(companyId, role.getRoleId())) {
+			ResourcePermission resourcePermission =
+				_resourcePermissionLocalService.getResourcePermission(
+					companyId, className, ResourceConstants.SCOPE_INDIVIDUAL,
+					String.valueOf(classPK), ownerRole.getRoleId());
+
+			return resourcePermission.getOwnerId() + StringPool.DASH +
+				   role.getRoleId();
+		}
+		else {
+			return String.valueOf(role.getRoleId());
+		}
+	}
+
+	private boolean _isOwnerRoleId(long companyId, long roleId) {
+		Role ownerRole = _roleLocalService.fetchRole(
+			companyId, RoleConstants.OWNER);
+
+		if ((ownerRole != null) && (roleId == ownerRole.getRoleId())) {
+			return true;
+		}
+
+		return false;
+	}
+
+	@Reference
+	private ResourcePermissionLocalService _resourcePermissionLocalService;
+
+	@Reference
+	private RoleLocalService _roleLocalService;
+}
