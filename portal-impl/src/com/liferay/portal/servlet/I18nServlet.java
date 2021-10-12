f6b82591fd9add7144eed1dc6cb6360f663437cc
Eric Yan <eric.yan@liferay.com>
2021-10-12 12:47:54 -0700

    LPS-134601 SF

--- portal-impl/src/com/liferay/portal/servlet/I18nServlet.java
+++ portal-impl/src/com/liferay/portal/servlet/I18nServlet.java
@@ -347,7 +347,8 @@ public class I18nServlet extends HttpServlet {
 		_setRequestAttributes(
 			httpServletRequest, httpServletResponse, i18nData);
 
-		String languageId = LocaleUtil.toW3cLanguageId(i18nData.getLanguageId());
+		String languageId = LocaleUtil.toW3cLanguageId(
+			i18nData.getLanguageId());
 
 		httpServletResponse.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
 
@@ -356,8 +357,8 @@ public class I18nServlet extends HttpServlet {
 		httpServletResponse.setHeader(
 			"Location",
 			StringBundler.concat(
-				servletContext.getContextPath(), StringPool.SLASH,
-				languageId, i18nData.getPath()));
+				servletContext.getContextPath(), StringPool.SLASH, languageId,
+				i18nData.getPath()));
 	}
 
 	private void _setRequestAttributes(
