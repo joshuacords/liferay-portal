6e3419d0d18f1ffc8a95dfa9a6c1b0014a8a76be
Jorge Díaz <jorge.diaz@liferay.com>
2021-10-18 08:25:24 +0200

    LPS-139947 Fail in case we cannot retrieve the sorted field from the last document

--- modules/apps/segments/segments-service/src/main/java/com/liferay/segments/internal/odata/search/ODataSearchAdapterImpl.java
+++ modules/apps/segments/segments-service/src/main/java/com/liferay/segments/internal/odata/search/ODataSearchAdapterImpl.java
@@ -238,6 +238,11 @@ public class ODataSearchAdapterImpl implements ODataSearchAdapter {
 			return booleanQuery;
 		}
 
+		if (!lastDocument.hasField(sortField)) {
+			throw new IllegalArgumentException(
+				"Missing " + sortField + " in last document");
+		}
+
 		BooleanQuery booleanQueryFromLastDocument = new BooleanQueryImpl();
 
 		booleanQueryFromLastDocument.add(booleanQuery, BooleanClauseOccur.MUST);
