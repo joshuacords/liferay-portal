0c0d676f8ec07191b28b98bb5a46681c2b808bd2
tamas.molnar <tamas.molnar@liferay.com>
2021-11-11 10:08:06 +0100

    LPS-132444 1. LPS-132444 Introduced ThreadLocal cache to speed up import. Enabled on JournalArticle and DLFileEntry 2. LPS-132444 Clean up and apply generally for all portlet data handlers

--- portal-kernel/src/com/liferay/exportimport/kernel/lar/PortletDataContext.java
+++ portal-kernel/src/com/liferay/exportimport/kernel/lar/PortletDataContext.java
@@ -633,6 +633,8 @@ public interface PortletDataContext extends Serializable {
 
 	public void setGroupId(long groupId);
 
+	public void setImportDataElementCacheEnabled(boolean enabled);
+
 	public void setImportDataRootElement(Element importDataRootElement);
 
 	public void setLayoutIds(long[] layoutIds);
