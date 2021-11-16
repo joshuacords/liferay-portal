68f4b4d6fa0e4199bcfa7b61ee6ee90d28b2936a
kylembischof <kyle.bischof@liferay.com>
2021-11-16 13:58:59 -0800

    Revert "ISSUP-4181 update account modified date when partner team contact change"
    This reverts commit 2c20646803dfea30fa53d400fcfe2c20e41cfaf1.

--- modules/dxp/apps/osb/osb-koroneiki/osb-koroneiki-taproot/osb-koroneiki-taproot-service/src/main/java/com/liferay/osb/koroneiki/taproot/internal/model/listener/ContactTeamRoleModelListener.java
+++ modules/dxp/apps/osb/osb-koroneiki/osb-koroneiki-taproot/osb-koroneiki-taproot-service/src/main/java/com/liferay/osb/koroneiki/taproot/internal/model/listener/ContactTeamRoleModelListener.java
@@ -42,12 +42,16 @@ public class ContactTeamRoleModelListener
 	public void onAfterCreate(ContactTeamRole contactTeamRole)
 		throws ModelListenerException {
 
+		_updateAccountModifiedDate(contactTeamRole);
+
 		_reindex(contactTeamRole);
 	}
 
 	@Override
 	public void onBeforeRemove(ContactTeamRole contactTeamRole)
 		throws ModelListenerException {
+
+		_updateAccountModifiedDate(contactTeamRole);
 
 		_reindex(contactTeamRole);
 	}
@@ -56,22 +60,30 @@ public class ContactTeamRoleModelListener
 		throws ModelListenerException {
 
 		try {
-			Team team = contactTeamRole.getTeam();
-
-			_accountLocalService.updateAccount(team.getAccount());
-
 			List<TeamAccountRole> teamAccountRoles =
 				_teamAccountRoleLocalService.getTeamAccountRoles(
 					contactTeamRole.getTeamId());
 
 			for (TeamAccountRole teamAccountRole : teamAccountRoles) {
-				_accountLocalService.updateAccount(
-					teamAccountRole.getAccount());
+				_accountLocalService.reindex(teamAccountRole.getAccountId());
 			}
 
 			_contactLocalService.reindex(contactTeamRole.getContactId());
 
 			_teamLocalService.reindex(contactTeamRole.getTeamId());
+		}
+		catch (PortalException portalException) {
+			throw new ModelListenerException(portalException);
+		}
+	}
+
+	private void _updateAccountModifiedDate(ContactTeamRole contactTeamRole)
+		throws ModelListenerException {
+
+		try {
+			Team team = contactTeamRole.getTeam();
+
+			_accountLocalService.updateAccount(team.getAccount());
 		}
 		catch (PortalException portalException) {
 			throw new ModelListenerException(portalException);
