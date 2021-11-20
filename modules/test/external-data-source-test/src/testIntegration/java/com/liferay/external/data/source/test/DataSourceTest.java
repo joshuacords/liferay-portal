a883f56dd308b250aa02c1cb9dcb5c7d582575ec
shuyangzhou <shuyang.zhou@liferay.com>
2021-11-19 16:59:28 -0800

    LPS-141790 Add test to expose upgrade process is still using portal datasource.

--- modules/test/external-data-source-test/src/testIntegration/java/com/liferay/external/data/source/test/DataSourceTest.java
+++ modules/test/external-data-source-test/src/testIntegration/java/com/liferay/external/data/source/test/DataSourceTest.java
@@ -52,6 +52,20 @@ public class DataSourceTest {
 	public void testUpdate() throws Exception {
 		try (Connection con = _dataSource.getConnection();
 			PreparedStatement ps = con.prepareStatement(
+				"select * from TestEntity");
+			ResultSet rs = ps.executeQuery()) {
+
+			Assert.assertTrue(
+				"Missing upgrade process created record", rs.next());
+
+			Assert.assertEquals(-1, rs.getLong("id_"));
+			Assert.assertEquals("Test Upgrade Value", rs.getString("data_"));
+
+			Assert.assertFalse("Found more than 1 record", rs.next());
+		}
+
+		try (Connection con = _dataSource.getConnection();
+			PreparedStatement ps = con.prepareStatement(
 				"delete from TestEntity")) {
 
 			ps.executeUpdate();
