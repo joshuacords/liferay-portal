14f62dcdca62bde7532657818360e0f3da2a2cf7
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2021-11-22 12:30:52 +0100

    LPS-142848 Add bind address option to be able to choose the network interface used.

--- util-java/src/com/liferay/util/transport/MulticastClientTool.java
+++ util-java/src/com/liferay/util/transport/MulticastClientTool.java
@@ -35,11 +35,12 @@ public class MulticastClientTool {
 		catch (Exception exception) {
 			exception.printStackTrace();
 
-			StringBuilder sb = new StringBuilder(3);
+			StringBuilder sb = new StringBuilder(4);
 
 			sb.append("Usage: java -classpath util-java.jar ");
 			sb.append(MulticastClientTool.class.getName());
-			sb.append("[-g] [-s] -h [multicastAddress] -p [port]");
+			sb.append("[-g] [-s] -h [multicastAddress] -p [port] [-b]");
+			sb.append("[bindAddress]");
 
 			System.err.println(sb.toString());
 
@@ -51,7 +52,8 @@ public class MulticastClientTool {
 		Map<String, Object> argsMap = _getArgsMap(args);
 
 		Integer port = (Integer)argsMap.get("port");
-		String host = (String)argsMap.get("host");
+		String multicastAddress = (String)argsMap.get("multicastAddress");
+		String bindAddress = (String)argsMap.get("bindAddress");
 
 		Boolean gzipData = (Boolean)argsMap.get("gzip");
 		Boolean shortData = (Boolean)argsMap.get("short");
@@ -60,7 +62,7 @@ public class MulticastClientTool {
 			gzipData.booleanValue(), shortData.booleanValue());
 
 		MulticastTransport multicastTransport = new MulticastTransport(
-			datagramHandler, host, port);
+			datagramHandler, multicastAddress, port, bindAddress);
 
 		if (shortData.booleanValue()) {
 			System.out.println("Truncating to 96 bytes.");
@@ -86,12 +88,17 @@ public class MulticastClientTool {
 				argsMap.put("short", Boolean.TRUE);
 			}
 			else if (args[i].equals("-h")) {
-				argsMap.put("host", args[i + 1]);
+				argsMap.put("multicastAddress", args[i + 1]);
 
 				i++;
 			}
 			else if (args[i].equals("-p")) {
 				argsMap.put("port", Integer.valueOf(args[i + 1]));
+
+				i++;
+			}
+			else if (args[i].equals("-b")) {
+				argsMap.put("bindAddress", args[i + 1]);
 
 				i++;
 			}
