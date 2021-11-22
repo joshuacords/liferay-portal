07cea79ec181f205773f12b103f61fbc63333ad1
Brian Chan <brian.chan@liferay.com>
2021-11-22 08:47:44 -0800

    LPS-142714 the method is getMode not getVideMode. Is this OK?

--- modules/apps/fragment/fragment-impl/src/main/java/com/liferay/fragment/internal/renderer/FragmentEntryFragmentRenderer.java
+++ modules/apps/fragment/fragment-impl/src/main/java/com/liferay/fragment/internal/renderer/FragmentEntryFragmentRenderer.java
@@ -139,7 +139,7 @@ public class FragmentEntryFragmentRenderer implements FragmentRenderer {
 
 	private String _renderFragmentEntry(
 		long fragmentEntryId, String css, String html, String js,
-		String configuration, String namespace, String viewMode,
+		String configuration, String namespace, String mode,
 		HttpServletRequest httpServletRequest) {
 
 		StringBundler sb = new StringBundler(16);
@@ -184,7 +184,7 @@ public class FragmentEntryFragmentRenderer implements FragmentRenderer {
 			}
 
 			if (!cssLoaded ||
-				Objects.equals(viewMode, FragmentEntryLinkConstants.EDIT)) {
+				Objects.equals(mode, FragmentEntryLinkConstants.EDIT)) {
 
 				sb.append("<style>");
 				sb.append(css);
