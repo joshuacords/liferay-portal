7a605586f5c6e36d80df47644cc396ec7685ea6c
clara izquierdo <claraizquierdosanz@gmail.com>
2021-12-09 13:26:10 +0100

    LPS-143212 Remove segmentsExperienceId parameter from simulation iframe

--- modules/apps/product-navigation/product-navigation-simulation-device/src/main/resources/META-INF/resources/js/product_navigation_simulation_device.js
+++ modules/apps/product-navigation/product-navigation-simulation-device/src/main/resources/META-INF/resources/js/product_navigation_simulation_device.js
@@ -360,6 +360,13 @@ AUI.add(
 							width
 						};
 
+						const url = new URL(WIN.location.href);
+						const searchParams = new URLSearchParams(url.search);
+						if (searchParams.has('segmentsExperienceId')) {
+							searchParams.delete('segmentsExperienceId');
+						}
+						searchParams.append('p_l_mode', 'preview');
+
 						Liferay.Util.openWindow(
 							{
 								cache: false,
@@ -370,10 +377,9 @@ AUI.add(
 								title: Liferay.Language.get(
 									'simulation-peview'
 								),
-								uri: Liferay.Util.addParams(
-									'p_l_mode=preview',
-									WIN.location.href
-								)
+								uri: `${url.origin}${
+									url.pathname
+								}?${searchParams.toString()}`,
 							},
 							dialogWindow => {
 								var dialogBoundingBox = dialogWindow.get(
