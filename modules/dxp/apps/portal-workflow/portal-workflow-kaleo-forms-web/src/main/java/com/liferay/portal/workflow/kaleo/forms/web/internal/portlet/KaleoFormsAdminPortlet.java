ea78d25f13e11ae5661690927dc56e4a7997ae31
Feliphe Marinho <feliphe.marinho@liferay.com>
2021-12-21 15:29:53 -0300

    LPS-144248 Avoid DoS attack

--- modules/dxp/apps/portal-workflow/portal-workflow-kaleo-forms-web/src/main/java/com/liferay/portal/workflow/kaleo/forms/web/internal/portlet/KaleoFormsAdminPortlet.java
+++ modules/dxp/apps/portal-workflow/portal-workflow-kaleo-forms-web/src/main/java/com/liferay/portal/workflow/kaleo/forms/web/internal/portlet/KaleoFormsAdminPortlet.java
@@ -66,6 +66,8 @@ import com.liferay.portal.kernel.transaction.Propagation;
 import com.liferay.portal.kernel.transaction.TransactionConfig;
 import com.liferay.portal.kernel.transaction.TransactionInvokerUtil;
 import com.liferay.portal.kernel.util.ContentTypes;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.LocalizationUtil;
 import com.liferay.portal.kernel.util.MimeTypesUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
@@ -108,7 +110,7 @@ import com.liferay.portal.workflow.kaleo.service.KaleoDefinitionVersionLocalServ
 import java.io.IOException;
 
 import java.util.ArrayList;
-import java.util.Enumeration;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -875,26 +877,25 @@ public class KaleoFormsAdminPortlet extends MVCPortlet {
 	 *
 	 * @param  resourceRequest the resource request
 	 * @param  resourceResponse the resource response
-	 * @throws Exception if an exception occurred
 	 */
 	protected void saveInPortletSession(
-			ResourceRequest resourceRequest, ResourceResponse resourceResponse)
-		throws Exception {
+		ResourceRequest resourceRequest, ResourceResponse resourceResponse) {
 
-		Enumeration<String> enumeration = resourceRequest.getParameterNames();
+		Map<String, String[]> parameterMap = resourceRequest.getParameterMap();
 
-		while (enumeration.hasMoreElements()) {
-			String name = enumeration.nextElement();
+		PortletSession portletSession = resourceRequest.getPortletSession();
 
-			if (Objects.equals(name, "doAsUserId")) {
+		for (String parameterName :
+				ListUtil.concat(
+					_getLocalizedParameterNames(), _parameterNames)) {
+
+			if (!parameterMap.containsKey(parameterName)) {
 				continue;
 			}
 
-			PortletSession portletSession = resourceRequest.getPortletSession();
-
-			String value = ParamUtil.getString(resourceRequest, name);
-
-			portletSession.setAttribute(name, value);
+			portletSession.setAttribute(
+				parameterName,
+				ParamUtil.getString(resourceRequest, parameterName));
 		}
 	}
 
@@ -1145,9 +1146,27 @@ public class KaleoFormsAdminPortlet extends MVCPortlet {
 	@Reference
 	protected StorageEngine storageEngine;
 
+	private List<String> _getLocalizedParameterNames() {
+		List<String> localizedParameters = new ArrayList<>();
+
+		for (Locale availableLocale : LanguageUtil.getAvailableLocales()) {
+			localizedParameters.add(
+				"description" + LocaleUtil.toLanguageId(availableLocale));
+			localizedParameters.add(
+				"name" + LocaleUtil.toLanguageId(availableLocale));
+		}
+
+		return localizedParameters;
+	}
+
 	private static final Log _log = LogFactoryUtil.getLog(
 		KaleoFormsAdminPortlet.class);
 
+	private static final List<String> _parameterNames = Arrays.asList(
+		"backURL", "ddmStructureId", "ddmStructureName", "ddmTemplateId",
+		"historyKey", "kaleoProcessId", "kaleoTaskFormPairsData", "mvcPath",
+		"redirect", "translatedLanguagesDescription", "translatedLanguagesName",
+		"workflowDefinition");
 	private static final TransactionConfig _transactionConfig =
 		TransactionConfig.Factory.create(
 			Propagation.REQUIRES_NEW, new Class<?>[] {Exception.class});
