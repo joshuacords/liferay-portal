0ebe757582a5495f4a3ac795894b3e5ff698964d
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2022-02-01 16:31:15 +0100

    LPS-146536 Return decodePortletName to avoid NullPointerException if portlet is undeployed.

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -4749,6 +4749,10 @@ public class PortalImpl implements Portal {
 	public String getPortletTitle(String portletId, Locale locale) {
 		PortletConfig portletConfig = PortletConfigFactoryUtil.get(portletId);
 
+		if (portletConfig == null) {
+			return PortletIdCodec.decodePortletName(portletId);
+		}
+
 		return getPortletTitle(
 			portletId, portletConfig.getResourceBundle(locale));
 	}
