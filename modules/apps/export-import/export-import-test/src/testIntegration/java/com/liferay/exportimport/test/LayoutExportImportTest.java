767d3108e4e7f2e1ddfa2a4ff30fb7e8a13e51bb
ChrisKian <christopher.kian@liferay.com>
2022-02-09 09:27:06 -0800

    LPS-144446 Add cmd value to the parameterMap for this specific test case

--- modules/apps/export-import/export-import-test/src/testIntegration/java/com/liferay/exportimport/test/LayoutExportImportTest.java
+++ modules/apps/export-import/export-import-test/src/testIntegration/java/com/liferay/exportimport/test/LayoutExportImportTest.java
@@ -34,6 +34,7 @@ import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.Constants;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.service.test.ServiceTestUtil;
@@ -421,9 +422,15 @@ public class LayoutExportImportTest extends BaseExportImportTestCase {
 		List<Layout> layouts = LayoutLocalServiceUtil.getLayouts(
 			group.getGroupId(), false);
 
-		exportImportLayouts(
+		Map<String, String[]> exportParameterMap = getExportParameterMap();
+
+		exportParameterMap.put(Constants.CMD, new String[] {Constants.EXPORT});
+
+		exportLayouts(
 			ExportImportHelperUtil.getLayoutIds(selectedLayouts),
-			getImportParameterMap());
+			exportParameterMap);
+
+		importLayouts(getImportParameterMap());
 
 		Assert.assertNotEquals(
 			layouts.size(),
