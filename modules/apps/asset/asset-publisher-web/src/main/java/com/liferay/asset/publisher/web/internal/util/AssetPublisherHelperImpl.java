d02589581eff096c4ce6f0aee105ba59a99091ff
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2022-02-18 19:30:01 +0100

    LPS-145876 Simplify

--- modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/internal/util/AssetPublisherHelperImpl.java
+++ modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/internal/util/AssetPublisherHelperImpl.java
@@ -500,14 +500,14 @@ public class AssetPublisherHelperImpl implements AssetPublisherHelper {
 			liferayPortletRequest, liferayPortletResponse, assetRenderer,
 			assetEntry);
 
-		String viewURL = null;
-
 		try {
-			String noSuchEntryRedirect = viewFullContentURL.toString();
-
-			viewURL = assetRenderer.getURLViewInContext(
+			String viewURL = assetRenderer.getURLViewInContext(
 				liferayPortletRequest, liferayPortletResponse,
-				noSuchEntryRedirect);
+				viewFullContentURL.toString());
+
+			if (Validator.isNotNull(viewURL)) {
+				return viewURL;
+			}
 		}
 		catch (Exception exception) {
 			if (_log.isDebugEnabled()) {
@@ -515,11 +515,7 @@ public class AssetPublisherHelperImpl implements AssetPublisherHelper {
 			}
 		}
 
-		if (Validator.isNull(viewURL)) {
-			viewURL = viewFullContentURL.toString();
-		}
-
-		return viewURL;
+		return viewFullContentURL.toString();
 	}
 
 	@Override
