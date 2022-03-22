3db0fd66e980188339b2b898451ac08b1b40fc5e
pei-jung <pei-jung.lan@liferay.com>
2022-03-21 17:08:07 -0700

    LPS-143898 UserGroup pages VIEW permission may be granted separately, so don't return yet and check for individual permission

--- portal-impl/src/com/liferay/portal/service/permission/LayoutPermissionImpl.java
+++ portal-impl/src/com/liferay/portal/service/permission/LayoutPermissionImpl.java
@@ -591,8 +591,6 @@ public class LayoutPermissionImpl
 				if (count >= 0) {
 					return true;
 				}
-
-				return false;
 			}
 			catch (PortalException | RuntimeException exception) {
 				throw exception;
