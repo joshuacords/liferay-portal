526bb8ab8d2f140a76473b5aa9f34330b2cae244
Alan Huang <alan.huang@liferay.com>
2022-04-26 01:08:20 +0800

    LPS-152014 Auto SF

--- modules/apps/static/portal-configuration/portal-configuration-persistence-test/src/testIntegration/java/com/liferay/portal/configuration/persistence/test/ConfigurationPersistenceManagerTest.java
+++ modules/apps/static/portal-configuration/portal-configuration-persistence-test/src/testIntegration/java/com/liferay/portal/configuration/persistence/test/ConfigurationPersistenceManagerTest.java
@@ -58,7 +58,7 @@ public class ConfigurationPersistenceManagerTest {
 	@Test
 	public void testConfigurationOverride() throws IOException {
 
-		// Override nonexisting configuration
+		// Override nonexistent configuration
 
 		String testPid = "testPid";
 
