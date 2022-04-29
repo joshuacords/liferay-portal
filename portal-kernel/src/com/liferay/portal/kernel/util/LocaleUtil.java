81b24b068189c076c597a7997e5df299f480bed7
Jorge Díaz <jorge.diaz@liferay.com>
2022-04-29 16:53:22 +0200

    LPS-152595 Don't append the country if it is blank

--- portal-kernel/src/com/liferay/portal/kernel/util/LocaleUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/LocaleUtil.java
@@ -342,7 +342,9 @@ public class LocaleUtil {
 
 		String displayName = null;
 
-		if (duplicateLanguages.contains(locale.getLanguage())) {
+		if (duplicateLanguages.contains(locale.getLanguage()) &&
+			Validator.isNotNull(country)) {
+
 			displayName = StringUtil.appendParentheticalSuffix(
 				language, country);
 		}
