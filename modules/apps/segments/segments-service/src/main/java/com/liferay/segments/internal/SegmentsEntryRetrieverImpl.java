69f0eb7c4ac6bb490222b84d3303933c2f49b34c
Cristina González Castellano <cristina.gonzalez@liferay.com>
2022-05-24 08:25:46 +0100

    LPS-154265 Implement the new method.

--- modules/apps/segments/segments-service/src/main/java/com/liferay/segments/internal/SegmentsEntryRetrieverImpl.java
+++ modules/apps/segments/segments-service/src/main/java/com/liferay/segments/internal/SegmentsEntryRetrieverImpl.java
@@ -46,7 +46,7 @@ public class SegmentsEntryRetrieverImpl implements SegmentsEntryRetriever {
 
 	@Override
 	public long[] getSegmentsEntryIds(
-		long groupId, long userId, Context context) {
+		long groupId, long userId, Context context, long[] segmentEntryIds) {
 
 		Optional<long[]> segmentsEntryIdsOptional =
 			_getSegmentsEntryIdsOptional();
@@ -64,7 +64,8 @@ public class SegmentsEntryRetrieverImpl implements SegmentsEntryRetriever {
 					try {
 						return _segmentsEntryProviderRegistry.
 							getSegmentsEntryIds(
-								groupId, User.class.getName(), userId, context);
+								groupId, User.class.getName(), userId, context,
+								segmentEntryIds);
 					}
 					catch (PortalException portalException) {
 						if (_log.isWarnEnabled()) {
