e74424af40291faa5767db947584f6f54a3e90f8
Jorge Díaz <jorge.diaz@liferay.com>
2022-06-22 15:21:48 +0200

    LPS-156725 Use the correct primKey when creating ResourcePermissions with group scope. In some children classes getPrimKey() is redefined and it doesn't return the groupId

--- modules/apps/portal-security/portal-security-permission-test-util/src/main/java/com/liferay/portal/security/permission/test/util/BasePermissionTestCase.java
+++ modules/apps/portal-security/portal-security-permission-test-util/src/main/java/com/liferay/portal/security/permission/test/util/BasePermissionTestCase.java
@@ -70,7 +70,7 @@ public abstract class BasePermissionTestCase {
 	protected void addPortletModelViewPermission() throws Exception {
 		RoleTestUtil.addResourcePermission(
 			getRoleName(), getResourceName(), ResourceConstants.SCOPE_GROUP,
-			getPrimKey(), ActionKeys.VIEW);
+			String.valueOf(group.getGroupId()), ActionKeys.VIEW);
 
 		Role role = RoleLocalServiceUtil.getRole(
 			TestPropsValues.getCompanyId(), getRoleName());
@@ -96,7 +96,7 @@ public abstract class BasePermissionTestCase {
 	protected void removePortletModelViewPermission() throws Exception {
 		RoleTestUtil.removeResourcePermission(
 			getRoleName(), getResourceName(), ResourceConstants.SCOPE_GROUP,
-			getPrimKey(), ActionKeys.VIEW);
+			String.valueOf(group.getGroupId()), ActionKeys.VIEW);
 
 		RoleTestUtil.removeResourcePermission(
 			getRoleName(), getResourceName(),
