2a3c770b88a3a394822de1ebd80ea3ad224a2537
Joshua Cords <joshua.cords@liferay.com>
2022-07-08 10:22:11 -0700

    LPS-153543 search-experiences-test: update tests
    https://issues.liferay.com/browse/LPS-153543

--- modules/dxp/apps/search-experiences/search-experiences-test/src/testIntegration/java/com/liferay/search/experiences/internal/blueprint/test/SXPBlueprintSearchResultTest.java
+++ modules/dxp/apps/search-experiences/search-experiences-test/src/testIntegration/java/com/liferay/search/experiences/internal/blueprint/test/SXPBlueprintSearchResultTest.java
@@ -1480,15 +1480,21 @@ public class SXPBlueprintSearchResultTest {
 
 	@Test
 	public void testLimitSearchToTheCurrentSite() throws Exception {
-		_addGroupAAndGroupB();
+		Group groupA = _addGroup();
 
-		_setUpJournalArticles(
-			new String[] {"", "", ""},
-			new String[] {"Site A", "Site B", "Current Site"});
+		_journalArticleBuilder.setTitle(
+			"Site A"
+		).setGroup(
+			groupA
+		).build();
+
+		_journalArticleBuilder.setTitle(
+			"Current Site"
+		).build();
 
 		_keywords = "Site";
 
-		User user = UserTestUtil.addUser(_groupA.getGroupId());
+		User user = UserTestUtil.addUser(groupA.getGroupId());
 
 		_serviceContext.setUserId(user.getUserId());
 
@@ -1499,7 +1505,7 @@ public class SXPBlueprintSearchResultTest {
 
 		_updateElementInstancesJSON(null, null);
 
-		_assertSearchIgnoreRelevance("[Current Site, Site A, Site B]");
+		_assertSearchIgnoreRelevance("[Current Site, Site A]");
 	}
 
 	@Test
