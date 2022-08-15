51e8a1d57a3e0bc2e160dd164779a17735d60fc8
Feliphe Marinho <feliphe.marinho@liferay.com>
2022-08-15 13:41:01 -0300

    LPS-160417 Revert "LPS-149303 For DSTShift method use displayTimeZone instead of calendarBooking.getTimeZone() when the booking is not all day"
    This reverts commit 0fc776372cd913ac8cc3c8a999b953383186e0de.

--- modules/apps/calendar/calendar-api/src/main/java/com/liferay/calendar/util/CalendarBookingIterator.java
+++ modules/apps/calendar/calendar-api/src/main/java/com/liferay/calendar/util/CalendarBookingIterator.java
@@ -107,9 +107,7 @@ public class CalendarBookingIterator implements Iterator<CalendarBooking> {
 			_getTimeZone(_calendarBooking));
 
 		int shift = JCalendarUtil.getDSTShift(
-			jCalendar, startTimeJCalendar,
-			_calendarBooking.isAllDay() ? TimeZone.getTimeZone(StringPool.UTC) :
-				_displayTimeZone);
+			jCalendar, startTimeJCalendar, _getTimeZone(_calendarBooking));
 
 		startTimeJCalendar.add(Calendar.MILLISECOND, shift);
 
