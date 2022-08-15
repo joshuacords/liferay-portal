12558828db93dc76a25f95f06401b73b656bbc82
Feliphe Marinho <feliphe.marinho@liferay.com>
2022-08-15 13:33:06 -0300

    LPS-160417 Revert "LPS-149303 Not needed"
    This reverts commit 5acb1cedca816e73937445f3f57ab6b037fc9d83.

--- modules/apps/calendar/calendar-test/src/testIntegration/java/com/liferay/calendar/upgrade/v4_1_3/test/UpgradeCalendarBookingTest.java
+++ modules/apps/calendar/calendar-test/src/testIntegration/java/com/liferay/calendar/upgrade/v4_1_3/test/UpgradeCalendarBookingTest.java
@@ -47,6 +47,7 @@ import org.junit.runner.RunWith;
 /**
  * @author István András Dézsi
  */
+@DataGuard(scope = DataGuard.Scope.METHOD)
 @RunWith(Arquillian.class)
 public class UpgradeCalendarBookingTest {
 
