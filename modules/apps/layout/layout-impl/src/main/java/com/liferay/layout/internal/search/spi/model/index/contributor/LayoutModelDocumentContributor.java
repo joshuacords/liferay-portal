9b5f290503ed4945db3717e959379ddf780850da
Brian Chan <brian.chan@liferay.com>
2022-09-07 11:36:41 -0300

    LPS-162222 Sort

--- modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/search/spi/model/index/contributor/LayoutModelDocumentContributor.java
+++ modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/search/spi/model/index/contributor/LayoutModelDocumentContributor.java
@@ -400,6 +400,10 @@ public class LayoutModelDocumentContributor
 					new MockHttpServletRequest(), "p_l_id=" + _layout.getPlid(),
 					false);
 
+			httpServletRequest.setAttribute(
+				WebKeys.COMPANY_ID, Long.valueOf(_layout.getCompanyId()));
+			httpServletRequest.setAttribute(WebKeys.LAYOUT, _layout);
+
 			ThemeDisplay themeDisplay = _getThemeDisplay();
 
 			themeDisplay.setLanguageId(LocaleUtil.toLanguageId(locale));
@@ -410,9 +414,6 @@ public class LayoutModelDocumentContributor
 			httpServletRequest.setAttribute(
 				WebKeys.THEME_DISPLAY, themeDisplay);
 
-			httpServletRequest.setAttribute(
-				WebKeys.COMPANY_ID, Long.valueOf(_layout.getCompanyId()));
-			httpServletRequest.setAttribute(WebKeys.LAYOUT, _layout);
 			httpServletRequest.setAttribute(WebKeys.USER, _user);
 			httpServletRequest.setAttribute(WebKeys.USER_ID, _user.getUserId());
 
