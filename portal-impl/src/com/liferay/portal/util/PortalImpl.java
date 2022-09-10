db01bceed42c090fb1256f7e44ffc80722da6759
Brian Chan <brian.chan@liferay.com>
2022-09-10 08:47:32 -0300

    LPS-158806 SF

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -913,12 +913,11 @@ public class PortalImpl implements Portal {
 				continue;
 			}
 
-			String[] renderParameter = renderParameters.get(
+			String[] values = renderParameters.get(
 				actionResponse.getNamespace() + actionParameterName);
 
-			if (renderParameter == null) {
-				String[] values = actionRequest.getParameterValues(
-					actionParameterName);
+			if (values == null) {
+				values = actionRequest.getParameterValues(actionParameterName);
 
 				if (values == null) {
 					values = new String[0];
