32c487202fa08fd77821797018c41f4c27966c2a
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2022-09-12 08:34:43 +0200

    LPS-127978 make sure the number value is on default format when it is a integer so we can round it properly

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/Numeric/Numeric.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/Numeric/Numeric.es.js
@@ -30,10 +30,13 @@ class Numeric extends Component {
 		const inputElement = element.querySelector('input');
 		const {value} = inputElement;
 
-		if (dataType === 'integer' && value !isNaN(Number(value)) {
+		if (dataType === 'integer' && value && !isNaN(Number(value))) {
 			inputElement.value = Math.round(
 				value.replace(symbols.decimalSymbol, '.')
 			);
+		}
+		else if (dataType === 'integer' && value) {
+			inputElement.value = value.replace(symbols.decimalSymbol, '.');
 		}
 
 		const mask = createNumberMask(this.getMaskConfig(dataType));
