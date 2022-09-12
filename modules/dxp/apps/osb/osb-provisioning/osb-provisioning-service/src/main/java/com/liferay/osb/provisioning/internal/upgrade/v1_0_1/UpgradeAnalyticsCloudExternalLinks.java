fb6f8ed0aacd4979b96c22af3b866cda0eca9ea2
amosfong <amos.fong@liferay.com>
2022-09-12 15:03:16 -0700

    ISSUP-5399 move AC script to upgrade class

---
+++ modules/dxp/apps/osb/osb-provisioning/osb-provisioning-service/src/main/java/com/liferay/osb/provisioning/internal/upgrade/v1_0_1/UpgradeAnalyticsCloudExternalLinks.java
@@ -0,0 +1,95 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.provisioning.internal.upgrade.v1_0_1;
+
+import com.liferay.osb.koroneiki.phloem.rest.client.constants.ExternalLinkDomain;
+import com.liferay.osb.koroneiki.phloem.rest.client.constants.ExternalLinkEntityName;
+import com.liferay.osb.koroneiki.phloem.rest.client.dto.v1_0.Account;
+import com.liferay.osb.koroneiki.phloem.rest.client.dto.v1_0.ExternalLink;
+import com.liferay.osb.provisioning.koroneiki.web.service.AccountWebService;
+import com.liferay.osb.provisioning.koroneiki.web.service.ExternalLinkWebService;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+
+import java.util.List;
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Amos Fong
+ */
+@Component(service = UpgradeAnalyticsCloudExternalLinks.class)
+public class UpgradeAnalyticsCloudExternalLinks extends UpgradeProcess {
+
+	public void upgrade(Map<String, Integer> acWorkspaceIdMap)
+		throws Exception {
+
+		for (Map.Entry<String, Integer> entry : acWorkspaceIdMap.entrySet()) {
+			String corpProjectUuid = entry.getKey();
+
+			Account account = null;
+
+			if (corpProjectUuid.startsWith("KOR-")) {
+				account = _accountWebService.fetchAccount(corpProjectUuid);
+			}
+			else {
+				List<Account> accounts = _accountWebService.getAccounts(
+					ExternalLinkDomain.WEB,
+					ExternalLinkEntityName.WEB_CORP_PROJECT, corpProjectUuid, 1,
+					1);
+
+				if (!accounts.isEmpty()) {
+					account = accounts.get(0);
+				}
+			}
+
+			if (account == null) {
+				_log.error(
+					"Unable to find account with key: " + corpProjectUuid);
+
+				continue;
+			}
+
+			ExternalLink externalLink = new ExternalLink();
+
+			externalLink.setDomain(ExternalLinkDomain.ANALYTICS_CLOUD);
+			externalLink.setEntityName(
+				ExternalLinkEntityName.ANALYTICS_CLOUD_GROUP);
+			externalLink.setEntityId(String.valueOf(entry.getValue()));
+
+			_externalLinkWebService.addAccountExternalLink(
+				StringPool.BLANK, StringPool.BLANK, account.getKey(),
+				externalLink);
+		}
+	}
+
+	@Override
+	protected void doUpgrade() {
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		UpgradeAnalyticsCloudExternalLinks.class);
+
+	@Reference
+	private AccountWebService _accountWebService;
+
+	@Reference
+	private ExternalLinkWebService _externalLinkWebService;
+
+}
