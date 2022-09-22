9f5bb081fc96f200fc6014f5a821ff41246b0234
Brian Chan <brian.chan@liferay.com>
2022-09-22 12:31:46 -0300

    LPS-163785 rename

--- modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/search/spi/model/index/contributor/LayoutModelDocumentContributor.java
+++ modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/search/spi/model/index/contributor/LayoutModelDocumentContributor.java
@@ -162,7 +162,7 @@ public class LayoutModelDocumentContributor
 		Set<Locale> locales = LanguageUtil.getAvailableLocales(
 			layout.getGroupId());
 
-		if (_useLayoutCrawler(layout)) {
+		if (_isUseLayoutCrawler(layout)) {
 			for (Locale locale : locales) {
 				String content = StringPool.BLANK;
 
@@ -396,7 +396,7 @@ public class LayoutModelDocumentContributor
 		return false;
 	}
 
-	private boolean _useLayoutCrawler(Layout layout) {
+	private boolean _isUseLayoutCrawler(Layout layout) {
 		if (layout.isPrivateLayout()) {
 			return false;
 		}
