106ac6b4f48a53941b6533296196ce115e4fafef
Matija Petanjek <matija.petanjek@liferay.com>
2022-09-26 23:41:32 +0200

    LPS-137769 portal-vulcan - if X-Forwarded host and port are present, use them when building server URL for API explorer

--- modules/apps/portal-vulcan/portal-vulcan-api/src/main/java/com/liferay/portal/vulcan/util/UriInfoUtil.java
+++ modules/apps/portal-vulcan/portal-vulcan-api/src/main/java/com/liferay/portal/vulcan/util/UriInfoUtil.java
@@ -39,6 +39,20 @@ public class UriInfoUtil {
 		return String.valueOf(uriInfo.getAbsolutePath());
 	}
 
+	public static String getBasePath(
+		String host, String scheme, UriInfo uriInfo) {
+
+		UriBuilder uriBuilder = getBaseUriBuilder(uriInfo);
+
+		uriBuilder.host(
+			host
+		).scheme(
+			scheme
+		);
+
+		return String.valueOf(uriBuilder.build());
+	}
+
 	public static String getBasePath(UriInfo uriInfo) {
 		UriBuilder uriBuilder = getBaseUriBuilder(uriInfo);
 
