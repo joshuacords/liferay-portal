45f03eab62731354f7191716c51d72200b5191d5
balazssk <balazs.safrany-kovalik@liferay.com>
2022-10-10 09:24:49 +0200

    LPS-118241 Partial backport of "LPS-151284 SF, call toString instead of toJSONString"

--- modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/service/impl/FragmentEntryLinkLocalServiceImpl.java
+++ modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/service/impl/FragmentEntryLinkLocalServiceImpl.java
@@ -786,7 +786,7 @@ public class FragmentEntryLinkLocalServiceImpl
 					defaultEditableFragmentEntryProcessorJSONObject);
 			}
 
-			return editableValuesJSONObject.toJSONString();
+			return editableValuesJSONObject.toString();
 		}
 		catch (JSONException jsonException) {
 			if (_log.isDebugEnabled()) {
