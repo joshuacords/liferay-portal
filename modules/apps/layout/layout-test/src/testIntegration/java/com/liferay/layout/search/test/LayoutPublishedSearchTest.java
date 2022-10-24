86bb699417698e8d8ae5975cda3dda02bb18c4de
balazssk <balazs.safrany-kovalik@liferay.com>
2022-10-24 10:46:02 +0200

    LPS-130057 Test - Perform function logic inline instead, since we cannot backport the methods due to portal-kernel minor version increase - 72x unique commit

--- modules/apps/layout/layout-test/src/testIntegration/java/com/liferay/layout/search/test/LayoutPublishedSearchTest.java
+++ modules/apps/layout/layout-test/src/testIntegration/java/com/liferay/layout/search/test/LayoutPublishedSearchTest.java
@@ -34,6 +34,7 @@ import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.util.FriendlyURLNormalizerUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.search.test.util.IndexerFixture;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
@@ -105,13 +106,15 @@ public class LayoutPublishedSearchTest {
 
 		ServiceContextThreadLocal.pushServiceContext(serviceContext);
 
+		Layout draftLayout = LayoutLocalServiceUtil.fetchLayout(
+			PortalUtil.getClassNameId(Layout.class), layout.getPlid());
+
 		ReflectionTestUtil.invoke(
 			_mvcActionCommand, "_publishLayout",
 			new Class<?>[] {
 				Layout.class, Layout.class, ServiceContext.class, long.class
 			},
-			layout.fetchDraftLayout(), layout, serviceContext,
-			TestPropsValues.getUserId());
+			draftLayout, layout, serviceContext, TestPropsValues.getUserId());
 	}
 
 	private void _setUpLayoutIndexerFixture() {
