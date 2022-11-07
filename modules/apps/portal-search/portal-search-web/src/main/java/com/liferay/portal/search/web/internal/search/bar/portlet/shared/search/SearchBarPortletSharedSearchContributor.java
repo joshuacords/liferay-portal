dc5637592a81b76df43e8e492eab3281cb0804b9
Georgel Pop <georgel.pop@liferay.com>
2022-11-07 13:10:15 +0100

    LPS-167763 Display correct results when permission scope is changed in a role

--- modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/search/bar/portlet/shared/search/SearchBarPortletSharedSearchContributor.java
+++ modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/search/bar/portlet/shared/search/SearchBarPortletSharedSearchContributor.java
@@ -89,11 +89,11 @@ public class SearchBarPortletSharedSearchContributor
 		SearchScope searchScope = getSearchScope(
 			searchBarPortletPreferences, portletSharedSearchSettings);
 
-		if (searchScope == SearchScope.THIS_SITE) {
-			searchRequestBuilder.withSearchContext(
-				searchContext -> searchContext.setGroupIds(
-					getGroupIds(portletSharedSearchSettings)));
+		searchRequestBuilder.withSearchContext(
+			searchContext -> searchContext.setGroupIds(
+				getGroupIds(portletSharedSearchSettings, searchScope)));
 
+		if (searchScope == SearchScope.THIS_SITE) {
 			return;
 		}
 
@@ -122,7 +122,8 @@ public class SearchBarPortletSharedSearchContributor
 	}
 
 	protected long[] getGroupIds(
-		PortletSharedSearchSettings portletSharedSearchSettings) {
+		PortletSharedSearchSettings portletSharedSearchSettings,
+		SearchScope searchScope) {
 
 		ThemeDisplay themeDisplay =
 			portletSharedSearchSettings.getThemeDisplay();
@@ -130,14 +131,20 @@ public class SearchBarPortletSharedSearchContributor
 		try {
 			List<Long> groupIds = new ArrayList<>();
 
-			groupIds.add(themeDisplay.getScopeGroupId());
+			if (searchScope == SearchScope.THIS_SITE) {
+				groupIds.add(themeDisplay.getScopeGroupId());
 
-			List<Group> groups = groupLocalService.getGroups(
-				themeDisplay.getCompanyId(), Layout.class.getName(),
-				themeDisplay.getScopeGroupId());
+				List<Group> groups = groupLocalService.getGroups(
+					themeDisplay.getCompanyId(), Layout.class.getName(),
+					themeDisplay.getScopeGroupId());
 
-			for (Group group : groups) {
-				groupIds.add(group.getGroupId());
+				for (Group group : groups) {
+					groupIds.add(group.getGroupId());
+				}
+			}
+			else {
+				groupIds = groupLocalService.getGroupIds(
+					themeDisplay.getCompanyId(), true);
 			}
 
 			return ArrayUtil.toLongArray(groupIds);
