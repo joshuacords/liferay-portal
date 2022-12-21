1243f0c2698abda6d3ef7b2f08c0cc5681009d79
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2022-12-21 16:26:23 +0700

    LPS-171240 We should check if the language id is a valid locale
    (cherry picked from commit 0bd1c0f324e259b9a55eecfebc64674b97a4bb2a)

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/portlet/action/UpdateArticleMVCActionCommand.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/portlet/action/UpdateArticleMVCActionCommand.java
@@ -51,6 +51,7 @@ import com.liferay.portal.kernel.upload.LiferayFileItemException;
 import com.liferay.portal.kernel.upload.UploadException;
 import com.liferay.portal.kernel.upload.UploadPortletRequest;
 import com.liferay.portal.kernel.util.Http;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.LocalizationUtil;
 import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
@@ -419,7 +420,8 @@ public class UpdateArticleMVCActionCommand extends BaseMVCActionCommand {
 		String referringPortletResource = ParamUtil.getString(
 			actionRequest, "referringPortletResource");
 
-		String languageId = ParamUtil.getString(actionRequest, "languageId");
+		String languageId = _getValidLanguageId(
+			ParamUtil.getString(actionRequest, "languageId"));
 
 		PortletURL portletURL = PortletURLFactoryUtil.create(
 			actionRequest, JournalPortletKeys.JOURNAL,
@@ -523,6 +525,20 @@ public class UpdateArticleMVCActionCommand extends BaseMVCActionCommand {
 		_journalContentSearchLocalService.updateContentSearch(
 			layout.getGroupId(), layout.isPrivateLayout(), layout.getLayoutId(),
 			portletResource, articleId, true);
+	}
+
+	private String _getValidLanguageId(String languageId) {
+		if (Validator.isNull(languageId)) {
+			return null;
+		}
+
+		Locale locale = LocaleUtil.fromLanguageId(languageId, true, false);
+
+		if (locale == null) {
+			return null;
+		}
+
+		return languageId;
 	}
 
 	private void _updateAssetEntryUsage(
