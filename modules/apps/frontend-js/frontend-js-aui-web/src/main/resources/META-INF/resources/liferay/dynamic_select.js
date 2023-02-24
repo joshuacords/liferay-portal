f6379da53eda859758642e69256f07ef7ed316b0
Stefano Motta <stefano.motta@liferay.com>
2023-02-24 18:10:27 +0100

    COMMERCE-5017 Fix portal taglibs used by commerce - frontend-js-aui-web

--- modules/apps/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/dynamic_select.js
+++ modules/apps/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/dynamic_select.js
@@ -124,8 +124,8 @@ AUI.add(
 				}
 
 				list.forEach(item => {
-					var key = item[selectId];
-					var value = item[selectDesc];
+					var key = Liferay.Util.escapeHTML(item[selectId]);
+					var value = Liferay.Util.escapeHTML(item[selectDesc]);
 
 					var selected = '';
 
