cb6f10bd7edbde9c7a2c63811ff2facd0a58ab28
Joshua Cords <joshua.cords@liferay.com>
2023-04-06 14:24:16 -0700

    journal-test: remove unnecessary code

--- modules/apps/journal/journal-test/src/testIntegration/java/com/liferay/journal/search/test/JournalArticlePermissionSearchTest.java
+++ modules/apps/journal/journal-test/src/testIntegration/java/com/liferay/journal/search/test/JournalArticlePermissionSearchTest.java
@@ -23,20 +23,13 @@ import com.liferay.journal.model.JournalFolder;
 import com.liferay.journal.test.util.JournalTestUtil;
 import com.liferay.portal.configuration.test.util.ConfigurationTestUtil;
 import com.liferay.portal.kernel.model.BaseModel;
-import com.liferay.portal.kernel.model.Role;
 import com.liferay.portal.kernel.module.configuration.ConfigurationProviderUtil;
 import com.liferay.portal.kernel.portlet.PortalPreferences;
 import com.liferay.portal.kernel.portlet.PortletPreferencesFactoryUtil;
-import com.liferay.portal.kernel.search.BooleanQuery;
-import com.liferay.portal.kernel.search.Hits;
-import com.liferay.portal.kernel.search.IndexSearcherHelperUtil;
-import com.liferay.portal.kernel.search.SearchContext;
 import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
 import com.liferay.portal.kernel.service.PortalPreferencesLocalServiceUtil;
-import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
-import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
 import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
@@ -46,7 +39,6 @@ import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
 import org.junit.After;
 import org.junit.AfterClass;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
@@ -89,9 +81,6 @@ public class JournalArticlePermissionSearchTest
 
 		CompanyThreadLocal.setCompanyId(TestPropsValues.getCompanyId());
 
-		ServiceContext serviceContext =
-			ServiceContextTestUtil.getServiceContext();
-
 		serviceContext.setCompanyId(TestPropsValues.getCompanyId());
 
 		PortalPreferences portalPreferences =
@@ -100,8 +89,6 @@ public class JournalArticlePermissionSearchTest
 
 		_originalPortalPreferencesXML = PortletPreferencesFactoryUtil.toXML(
 			portalPreferences);
-
-		portalPreferences.setValue("", "articleCommentsEnabled", "true");
 
 		PortalPreferencesLocalServiceUtil.updatePreferences(
 			TestPropsValues.getCompanyId(),
@@ -129,24 +116,9 @@ public class JournalArticlePermissionSearchTest
 
 		JournalFolder folder = (JournalFolder)parentBaseModel;
 
-		long folderId = JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID;
-
-		if (folder != null) {
-			folderId = folder.getFolderId();
-		}
-
 		return JournalTestUtil.addArticleWithWorkflow(
-			group.getGroupId(), folderId, keywords,
+			group.getGroupId(), folder.getFolderId(), keywords,
 			RandomTestUtil.randomString(50), approved, serviceContext);
-	}
-
-	protected void assertEquals(
-			long length, BooleanQuery query, SearchContext searchContext)
-		throws Exception {
-
-		Hits hits = IndexSearcherHelperUtil.search(searchContext, query);
-
-		Assert.assertEquals(hits.toString(), length, hits.getLength());
 	}
 
 	@Override
@@ -167,21 +139,9 @@ public class JournalArticlePermissionSearchTest
 
 		JournalFolder folder = (JournalFolder)parentBaseModel;
 
-		long folderId = JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID;
-
-		if (folder != null) {
-			folderId = folder.getFolderId();
-		}
-
 		return JournalTestUtil.addFolder(
-			folderId, RandomTestUtil.randomString(), serviceContext);
-	}
-
-	@Override
-	protected BaseModel<?> getParentBaseModel(Role role) throws Exception {
-		return JournalTestUtil.addFolder(
-			JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID,
-			RandomTestUtil.randomString(), serviceContext);
+			folder.getFolderId(), RandomTestUtil.randomString(),
+			serviceContext);
 	}
 
 	@Override
