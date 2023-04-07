1da37125ce27f908c1d8b826d16f7e9c1c2f420c
Joshua Cords <joshua.cords@liferay.com>
2023-04-07 09:33:16 -0700

    portal-kernel: add inheritedPermissionIndexing

--- portal-kernel/src/com/liferay/portal/kernel/search/SearchResultPermissionFilterFactory.java
+++ portal-kernel/src/com/liferay/portal/kernel/search/SearchResultPermissionFilterFactory.java
@@ -26,4 +26,6 @@ public interface SearchResultPermissionFilterFactory {
 			searchResultPermissionFilterSearcher,
 		PermissionChecker permissionChecker);
 
+	public boolean inheritedPermissionIndexing();
+
 }
