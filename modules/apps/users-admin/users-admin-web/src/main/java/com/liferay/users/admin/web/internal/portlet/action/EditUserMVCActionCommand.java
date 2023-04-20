97941ede492a8c5150b32431b1c9bbaf1ee0a59c
Tina Tian <tina.tian@liferay.com>
2023-04-20 14:44:15 -0700

    LPS-161481 Should only validate max company users when necessary, the validate logic was added to UserLocalServiceImpl by LPS-29673(46c7bdfe708dda4197b5f98605a776979f4a5176) to fix an issue when user status changed by RESTORE action, this should not be expanded to other cases by modifying local service logic.

--- modules/apps/users-admin/users-admin-web/src/main/java/com/liferay/users/admin/web/internal/portlet/action/EditUserMVCActionCommand.java
+++ modules/apps/users-admin/users-admin-web/src/main/java/com/liferay/users/admin/web/internal/portlet/action/EditUserMVCActionCommand.java
@@ -173,6 +173,12 @@ public class EditUserMVCActionCommand extends BaseMVCActionCommand {
 				WorkflowConstants.STATUS_INACTIVE);
 		}
 		else if (cmd.equals(Constants.RESTORE)) {
+			ThemeDisplay themeDisplay =
+				(ThemeDisplay)actionRequest.getAttribute(WebKeys.THEME_DISPLAY);
+
+			_userLocalService.validateCompanyMaxUsers(
+				themeDisplay.getCompanyId());
+
 			_updateUsers(
 				actionRequest, deleteUserIds,
 				WorkflowConstants.STATUS_APPROVED);
@@ -530,8 +536,6 @@ public class EditUserMVCActionCommand extends BaseMVCActionCommand {
 		}
 	}
 
-	}
-
 	private void _updateUsers(
 			ActionRequest actionRequest, long[] accountUserIds, int status)
 		throws Exception {
@@ -542,6 +546,7 @@ public class EditUserMVCActionCommand extends BaseMVCActionCommand {
 				ServiceContextFactory.getInstance(
 					User.class.getName(), actionRequest));
 		}
+	}
 
 	private ActionRequest _wrapActionRequest(ActionRequest actionRequest)
 		throws Exception {
@@ -564,6 +569,9 @@ public class EditUserMVCActionCommand extends BaseMVCActionCommand {
 
 	private DLAppLocalService _dlAppLocalService;
 	private ListTypeLocalService _listTypeLocalService;
+
+	@Reference
+	private UserLocalService _userLocalService;
 	private UserService _userService;
 
 }
--- portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
@@ -5209,12 +5209,6 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 
 		User user = userPersistence.findByPrimaryKey(userId);
 
-		if ((status == WorkflowConstants.STATUS_APPROVED) &&
-			(user.getStatus() != WorkflowConstants.STATUS_APPROVED)) {
-
-			validateCompanyMaxUsers(user.getCompanyId());
-		}
-
 		String passwordUnencrypted = (String)serviceContext.getAttribute(
 			"passwordUnencrypted");
 
@@ -5545,6 +5539,21 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 		}
 
 		return user;
+	}
+
+	public void validateCompanyMaxUsers(long companyId) throws PortalException {
+		Company company = companyPersistence.findByPrimaryKey(companyId);
+
+		if (company.isSystem() || (company.getMaxUsers() == 0)) {
+			return;
+		}
+
+		int userCount = searchCount(
+			companyId, null, WorkflowConstants.STATUS_APPROVED, null);
+
+		if (userCount >= company.getMaxUsers()) {
+			throw new CompanyMaxUsersException();
+		}
 	}
 
 	/**
@@ -6847,23 +6856,6 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 
 		if (Validator.isNotNull(smsSn) && !Validator.isEmailAddress(smsSn)) {
 			throw new UserSmsException.MustBeEmailAddress(smsSn);
-		}
-	}
-
-	protected void validateCompanyMaxUsers(long companyId)
-		throws PortalException {
-
-		Company company = companyPersistence.findByPrimaryKey(companyId);
-
-		if (company.isSystem() || (company.getMaxUsers() == 0)) {
-			return;
-		}
-
-		int userCount = searchCount(
-			companyId, null, WorkflowConstants.STATUS_APPROVED, null);
-
-		if (userCount >= company.getMaxUsers()) {
-			throw new CompanyMaxUsersException();
 		}
 	}
 
