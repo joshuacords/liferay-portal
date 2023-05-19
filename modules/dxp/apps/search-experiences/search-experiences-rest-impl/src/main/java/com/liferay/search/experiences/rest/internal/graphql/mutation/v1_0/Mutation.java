5594a106c1a0abf8a2a638b6b5d928a2c981d27d
Joshua Cords <joshua.cords@liferay.com>
2023-05-19 15:36:22 -0700

    LPS-178545 search-experiences: buildREST
    https://issues.liferay.com/browse/LPS-178545

--- modules/dxp/apps/search-experiences/search-experiences-rest-impl/src/main/java/com/liferay/search/experiences/rest/internal/graphql/mutation/v1_0/Mutation.java
+++ modules/dxp/apps/search-experiences/search-experiences-rest-impl/src/main/java/com/liferay/search/experiences/rest/internal/graphql/mutation/v1_0/Mutation.java
@@ -400,6 +400,18 @@ public class Mutation {
 	}
 
 	@GraphQLField
+	public SXPElement createSXPElementPreview(
+			@GraphQLName("string") String string)
+		throws Exception {
+
+		return _applyComponentServiceObjects(
+			_sxpElementResourceComponentServiceObjects,
+			this::_populateResourceContext,
+			sxpElementResource -> sxpElementResource.postSXPElementPreview(
+				string));
+	}
+
+	@GraphQLField
 	public SXPElement createSXPElementValidate(
 			@GraphQLName("string") String string)
 		throws Exception {
