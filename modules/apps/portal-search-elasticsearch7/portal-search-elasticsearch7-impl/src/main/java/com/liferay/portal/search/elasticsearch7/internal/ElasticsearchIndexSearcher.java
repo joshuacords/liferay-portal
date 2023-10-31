dd8cd3037d087803555296f2e47004a9d3b4036e
Joshua Cords <joshua.cords@liferay.com>
2023-10-31 12:50:09 -0700

    LPS-200536 portal-search-elasticsearch7-impl: add test
    https://liferay.atlassian.net/browse/LPS-200536

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/ElasticsearchIndexSearcher.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/ElasticsearchIndexSearcher.java
@@ -529,11 +529,13 @@ public class ElasticsearchIndexSearcher extends BaseIndexSearcher {
 			SearchSearchRequest searchSearchRequest = createSearchSearchRequest(
 				searchRequest, searchContext, query);
 
-			searchSearchRequest.setSize(
-				Math.min(
-					end - start,
-					_elasticsearchConfigurationWrapper.indexMaxResultWindow() -
-						start));
+			int size = Math.min(
+				end - start,
+				_elasticsearchConfigurationWrapper.indexMaxResultWindow() -
+					start);
+
+			searchSearchRequest.setSize(size);
+
 			searchSearchRequest.setSorts(searchContext.getSorts());
 			searchSearchRequest.setSorts(searchRequest.getSorts());
 			searchSearchRequest.setStart(start);
@@ -558,7 +560,7 @@ public class ElasticsearchIndexSearcher extends BaseIndexSearcher {
 
 			Document[] documents = hits.getDocs();
 
-			if ((documents.length != 0) || (start == 0)) {
+			if ((documents.length != 0) || (start == 0) || (size < 1)) {
 				break;
 			}
 
