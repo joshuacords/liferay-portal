879b8d6dcbe297dde2e75df704d415238ff23fa8
Joshua Cords <joshua.cords@liferay.com>
2024-04-24 11:17:44 -0700

    LPD-5525 portal-search-similar-results-web-spi: add javaDocs
    https://liferay.atlassian.net/browse/LPD-5525

--- modules/dxp/apps/portal-search-similar-results/portal-search-similar-results-web/src/main/java/com/liferay/portal/search/similar/results/web/internal/contributor/SimilarResultsContributor.java
+++ modules/dxp/apps/portal-search-similar-results/portal-search-similar-results-web/src/main/java/com/liferay/portal/search/similar/results/web/internal/contributor/SimilarResultsContributor.java
@@ -17,11 +17,42 @@ import com.liferay.portal.search.similar.results.web.spi.contributor.helper.Rout
  */
 public interface SimilarResultsContributor {
 
+	/**
+	 * This method is used to detect the type of similar results contributor to
+	 * apply, given the URL route of the originating asset. When the URL Route
+	 * matches their type of contributor, implementations should use the
+	 * provided {@link RouteBuilder} to add attributes necessary later for
+	 * {@link #resolveCriteria} from the URL available in the
+	 * {@link RouteHelper}.
+	 *
+	 * @param routeBuilder An instance of {@link RouteBuilder} to be used adding attributes.
+	 * @param routeHelper An instance of {@link RouteHelper} to be used to retrieve the URL.
+	 */
 	public void detectRoute(RouteBuilder routeBuilder, RouteHelper routeHelper);
 
+	/**
+	 * This method is used to add criteria of the originating asset in order to
+	 * run a search query based on that asset. Implementations should add the
+	 * asset's UID and (optionally) className to {@link CriteriaBuilder} and use
+	 * {@link CriteriaHelper} to access any attributes added to the routeBuilder
+	 * during {@link #detectRoute}.
+	 *
+	 * @param criteriaBuilder An instance of {@link CriteriaBuilder} to be used for adding any necessary criteria
+	 * @param criteriaHelper An instance of {@link CriteriaHelper} to be used to access any attributes added to the routeBuilder during {@link #detectRoute}.
+	 */
 	public void resolveCriteria(
 		CriteriaBuilder criteriaBuilder, CriteriaHelper criteriaHelper);
 
+	/**
+	 * This method is called for each similar result and is used to overwrite
+	 * parts of the destination URL for similar results. Implementations should
+	 * use the provided {@link DestinationBuilder} to modify the destination URL
+	 * and the {@link DestinationHelper} to retrieve necessary information about
+	 * the asset.
+	 *
+	 * @param destinationBuilder An instance of {@link DestinationBuilder} to be used for constructing the destination.
+	 * @param destinationHelper An instance of {@link DestinationHelper} to be used to retrieve necessary information about the asset.
+	 */
 	public void writeDestination(
 		DestinationBuilder destinationBuilder,
 		DestinationHelper destinationHelper);
