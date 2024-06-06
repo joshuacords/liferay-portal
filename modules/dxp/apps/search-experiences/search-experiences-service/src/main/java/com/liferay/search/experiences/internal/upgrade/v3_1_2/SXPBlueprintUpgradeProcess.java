56da05c413eeaf153bfa9871fc76ad5a3c18e4ae
Joshua Cords <joshua.cords@liferay.com>
2024-06-06 09:10:01 -0700

    LPD-22974 LPD-26515 search-experiences-service: add upgrade process
    https://liferay.atlassian.net/browse/LPD-26515

---
+++ modules/dxp/apps/search-experiences/search-experiences-service/src/main/java/com/liferay/search/experiences/internal/upgrade/v3_1_2/SXPBlueprintUpgradeProcess.java
@@ -0,0 +1,55 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.search.experiences.internal.upgrade.v3_1_2;
+
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.search.experiences.internal.util.UpgradeSXPBlueprintContributorStorageSchemaHelper;
+import com.liferay.search.experiences.model.SXPBlueprint;
+import com.liferay.search.experiences.service.SXPBlueprintLocalService;
+
+import java.util.List;
+
+/**
+ * @author Joshua Cords
+ */
+public class SXPBlueprintUpgradeProcess extends UpgradeProcess {
+
+	public SXPBlueprintUpgradeProcess(
+		CompanyLocalService companyLocalService,
+		SXPBlueprintLocalService sxpBlueprintLocalService,
+		UpgradeSXPBlueprintContributorStorageSchemaHelper
+			upgradeSXPBlueprintContributorStorageSchemaHelper) {
+
+		_companyLocalService = companyLocalService;
+		_sxpBlueprintLocalService = sxpBlueprintLocalService;
+		_upgradeSXPBlueprintContributorStorageSchemaHelper =
+			upgradeSXPBlueprintContributorStorageSchemaHelper;
+	}
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		_companyLocalService.forEachCompanyId(
+			companyId -> {
+				List<SXPBlueprint> sxpBlueprints =
+					_sxpBlueprintLocalService.getSXPBlueprints(companyId);
+
+				for (SXPBlueprint sxpBlueprint : sxpBlueprints) {
+					sxpBlueprint =
+						_upgradeSXPBlueprintContributorStorageSchemaHelper.
+							upgradeContributorStorageSchema(sxpBlueprint);
+
+					_sxpBlueprintLocalService.updateSXPBlueprint(sxpBlueprint);
+				}
+			});
+	}
+
+	private final CompanyLocalService _companyLocalService;
+	private final SXPBlueprintLocalService _sxpBlueprintLocalService;
+	private final UpgradeSXPBlueprintContributorStorageSchemaHelper
+		_upgradeSXPBlueprintContributorStorageSchemaHelper;
+
+}
