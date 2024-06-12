5e7549d2645c4435310123aef1dd2a65eed80c54
Joshua Cords <joshua.cords@liferay.com>
2024-06-11 17:58:44 -0700

    LPD-22974 LPD-27102 search-experiences-test: change package
    https://liferay.atlassian.net/browse/LPD-22974

--- modules/dxp/apps/search-experiences/search-experiences-test/src/testIntegration/java/com/liferay/search/experiences/internal/upgrade/v3_1_2/test/SXPBlueprintUpgradeProcessTest.java
+++ modules/dxp/apps/search-experiences/search-experiences-test/src/testIntegration/java/com/liferay/search/experiences/internal/upgrade/v3_1_2/test/SXPBlueprintUpgradeProcessTest.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.search.experiences.internal.blueprint.upgrade.v3_1_2.test;
+package com.liferay.search.experiences.internal.upgrade.v3_1_2.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.petra.string.StringBundler;
