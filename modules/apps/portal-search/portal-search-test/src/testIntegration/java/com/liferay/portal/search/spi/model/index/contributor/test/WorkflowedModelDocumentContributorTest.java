879c8b4296c548ec9381200add8e1e05059c7b20
Joshua Cords <joshua.cords@liferay.com>
2024-08-07 11:24:34 -0700

    LPD-28416 LPD-32921 portal-search-test: add test for WorkflowedModelDocumentContributor
    https://liferay.atlassian.net/browse/LPD-28416

---
+++ modules/apps/portal-search/portal-search-test/src/testIntegration/java/com/liferay/portal/search/spi/model/index/contributor/test/WorkflowedModelDocumentContributorTest.java
@@ -0,0 +1,34 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.search.spi.model.index.contributor.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Joshua Cords
+ */
+@RunWith(Arquillian.class)
+public class WorkflowedModelDocumentContributorTest
+	extends BaseModelDocumentContributorTest {
+
+	@Test
+	public void testContribute() throws Exception {
+		testContribute(
+			blogsEntry, user.getExternalReferenceCode(),
+			_STATUS_BY_USER_EXTERNAL_REFERENCE_CODE_FIELD_NAME);
+		testContribute(
+			journalArticle, user.getExternalReferenceCode(),
+			_STATUS_BY_USER_EXTERNAL_REFERENCE_CODE_FIELD_NAME);
+	}
+
+	private static final String
+		_STATUS_BY_USER_EXTERNAL_REFERENCE_CODE_FIELD_NAME =
+			"statusByUserExternalReferenceCode";
+
+}
