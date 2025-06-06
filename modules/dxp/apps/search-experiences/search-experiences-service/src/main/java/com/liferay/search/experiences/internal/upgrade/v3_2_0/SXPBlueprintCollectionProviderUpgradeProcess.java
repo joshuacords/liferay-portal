f5ef02539f6ae8436961c667c3b61ede45803bc0
Joshua Cords <joshua.cords@liferay.com>
2025-06-06 14:37:29 -0700

    LPD-56455 search-experiences-service: use sql directly as FeatureFlagUtil is not available using the upgrade tool
    https://liferay.atlassian.net/browse/LPD-56455

--- modules/dxp/apps/search-experiences/search-experiences-service/src/main/java/com/liferay/search/experiences/internal/upgrade/v3_2_0/SXPBlueprintCollectionProviderUpgradeProcess.java
+++ modules/dxp/apps/search-experiences/search-experiences-service/src/main/java/com/liferay/search/experiences/internal/upgrade/v3_2_0/SXPBlueprintCollectionProviderUpgradeProcess.java
@@ -5,8 +5,8 @@
 
 package com.liferay.search.experiences.internal.upgrade.v3_2_0;
 
+import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.kernel.dao.jdbc.AutoBatchPreparedStatementUtil;
-import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
@@ -31,11 +31,16 @@ public class SXPBlueprintCollectionProviderUpgradeProcess
 
 	@Override
 	protected void doUpgrade() throws Exception {
-		if (!FeatureFlagManagerUtil.isEnabled("LPS-129412")) {
-			return;
-		}
+		try (PreparedStatement preparedStatement1 = connection.prepareStatement(
+				"select companyId from PortalPreferenceValue where key_ = " +
+					"'LPS-129412' and smallValue = 'true'")) {
 
-		_upgradeSXPBlueprints();
+			try (ResultSet resultSet = preparedStatement1.executeQuery()) {
+				while (resultSet.next()) {
+					_upgradeSXPBlueprints(resultSet.getLong(1));
+				}
+			}
+		}
 	}
 
 	private String _setAsCollectionProvider(String configurationJSON)
@@ -60,14 +65,17 @@ public class SXPBlueprintCollectionProviderUpgradeProcess
 		return configurationJSONObject.toString();
 	}
 
-	private void _upgradeSXPBlueprints() throws Exception {
+	private void _upgradeSXPBlueprints(long companyId) throws Exception {
 		SXPBlueprintCollectionProviderHelper
 			sxpBlueprintCollectionProviderHelper =
 				new SXPBlueprintCollectionProviderHelper(
 					_assetSubtypeIdentifierBuilder);
 
 		try (PreparedStatement preparedStatement1 = connection.prepareStatement(
-				"select configurationJSON,sxpBlueprintId from SXPBlueprint");
+			StringBundler.concat(
+				"select configurationJSON, sxpBlueprintId from SXPBlueprint ",
+				"where companyId = ", companyId));
+
 			PreparedStatement preparedStatement2 =
 				AutoBatchPreparedStatementUtil.concurrentAutoBatch(
 					connection,
