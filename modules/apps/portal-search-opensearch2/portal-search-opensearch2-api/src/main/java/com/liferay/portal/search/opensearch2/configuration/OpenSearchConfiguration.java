7e3afbe19bf30fe4e77e22fecced9316390d4404
Joshua Cords <joshua.cords@liferay.com>
2025-09-11 16:24:06 -0700

    LPD-65371 portal-search-opensearch2-api: add trackTotalHitsUpTo configuration
    https://liferay.atlassian.net/browse/LPD-65371

--- modules/apps/portal-search-opensearch2/portal-search-opensearch2-api/src/main/java/com/liferay/portal/search/opensearch2/configuration/OpenSearchConfiguration.java
+++ modules/apps/portal-search-opensearch2/portal-search-opensearch2-api/src/main/java/com/liferay/portal/search/opensearch2/configuration/OpenSearchConfiguration.java
@@ -89,4 +89,10 @@ public interface OpenSearchConfiguration {
 	)
 	public boolean trackTotalHits();
 
+	@Meta.AD(
+		deflt = "2147483647", description = "track-total-hits-up-to-help",
+		max = "2147483647", name = "track-total-hits-up-to", required = false
+	)
+	public int trackTotalHitsUpTo();
+
 }
