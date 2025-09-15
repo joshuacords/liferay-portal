74a2315eb38c1909a19199fbf9e0b1b62884c2bc
Bryan Engler <bryan.engler@liferay.com>
2025-09-15 08:42:52 -0700

    LPD-61488 portal-search-elasticsearch7-impl: Dont need this any more, trackTotalHitsUpTo takes precedence
    https://liferay.atlassian.net/browse/LPD-61488

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/ElasticsearchIndexSearcher.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/ElasticsearchIndexSearcher.java
@@ -542,8 +542,6 @@ public class ElasticsearchIndexSearcher extends BaseIndexSearcher {
 			searchRequest.getPostFilterComplexQueryParts());
 		baseSearchRequest.setRescores(searchRequest.getRescores());
 		baseSearchRequest.setStatsRequests(searchRequest.getStatsRequests());
-		baseSearchRequest.setTrackTotalHits(
-			_elasticsearchConfigurationWrapper.trackTotalHits());
 
 		_setAggregations(baseSearchRequest, searchRequest);
 		_setConnectionId(baseSearchRequest, searchRequest);
