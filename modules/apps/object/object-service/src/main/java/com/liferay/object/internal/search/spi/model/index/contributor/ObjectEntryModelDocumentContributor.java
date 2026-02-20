eeb8a6de88e4c7d01c311044eda5319a24754d85
Joshua Cords <joshua.cords@liferay.com>
2026-02-19 16:42:08 -0800

    LPD-75868 object-service: create fallbacks for when localized objects do not have fields for the searched locale - mirrors current JournalArticle behavior
    https://liferay.atlassian.net/browse/LPD-75868

--- modules/apps/object/object-service/src/main/java/com/liferay/object/internal/search/spi/model/index/contributor/ObjectEntryModelDocumentContributor.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/search/spi/model/index/contributor/ObjectEntryModelDocumentContributor.java
@@ -386,6 +386,9 @@ public class ObjectEntryModelDocumentContributor
 			_log.debug("Object entry " + objectEntry);
 		}
 
+		document.addText(
+			"defaultLanguageId", objectEntry.getDefaultLanguageId());
+
 		document.add(
 			new Field(
 				Field.getSortableFieldName(Field.ENTRY_CLASS_PK),
@@ -471,7 +474,8 @@ public class ObjectEntryModelDocumentContributor
 
 				document.add(
 					new Field(
-						"objectEntryContent_" + entry.getKey(),
+						Field.getLocalizedName(
+							entry.getKey(), "objectEntryContent"),
 						entry.getValue()));
 			}
 
