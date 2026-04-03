c8587288e616a38abb7e8f8390dd617e4b5273fc
Joshua Cords <joshua.cords@liferay.com>
2026-04-02 23:04:52 -0700

    LPD-75868 WIP
    https://liferay.atlassian.net/browse/LPD-75868

--- modules/apps/portal-search-opensearch2/portal-search-opensearch2-impl/src/main/java/com/liferay/portal/search/opensearch2/internal/hits/HitsMetadataTranslator.java
+++ modules/apps/portal-search-opensearch2/portal-search-opensearch2-impl/src/main/java/com/liferay/portal/search/opensearch2/internal/hits/HitsMetadataTranslator.java
@@ -34,6 +34,7 @@ import org.opensearch.client.json.JsonData;
 import org.opensearch.client.opensearch.core.explain.Explanation;
 import org.opensearch.client.opensearch.core.search.Hit;
 import org.opensearch.client.opensearch.core.search.HitsMetadata;
+import org.opensearch.client.opensearch.core.search.InnerHitsResult;
 import org.opensearch.client.opensearch.core.search.TotalHits;
 
 /**
@@ -76,7 +77,7 @@ public class HitsMetadataTranslator {
 		SearchHitBuilder searchHitBuilder = new SearchHitBuilder();
 
 		return searchHitBuilder.addHighlightFields(
-			_translateHighlightFields(hit.highlight())
+			_translateHighlightFields(hit)
 		).addSources(
 			_translateSource(hit.source())
 		).document(
@@ -126,21 +127,55 @@ public class HitsMetadataTranslator {
 	}
 
 	private List<HighlightField> _translateHighlightFields(
-		Map<String, List<String>> highlight) {
+		Hit<JsonData> hit) {
 
 		List<HighlightField> highlightFields = new ArrayList<>();
 
-		for (Map.Entry<String, List<String>> entry : highlight.entrySet()) {
-			highlightFields.add(
-				new HighlightFieldBuilder(
-				).fragments(
-					entry.getValue()
-				).name(
-					entry.getKey()
-				).build());
-		}
+		_populateHighlightFields(hit, highlightFields);
 
 		return highlightFields;
+	}
+
+	private void _populateHighlightFields(
+		Hit<JsonData> hit, List<HighlightField> highlightFields) {
+
+		Map<String, List<String>> highlight = hit.highlight();
+
+		if (highlight != null) {
+			for (Map.Entry<String, List<String>> entry : highlight.entrySet()) {
+				highlightFields.add(
+					new HighlightFieldBuilder(
+					).fragments(
+						entry.getValue()
+					).name(
+						entry.getKey()
+					).build());
+			}
+		}
+
+		Map<String, InnerHitsResult> innerHits = hit.innerHits();
+
+		if ((innerHits == null) || innerHits.isEmpty()) {
+			return;
+		}
+
+		for (InnerHitsResult innerHitsResult : innerHits.values()) {
+			HitsMetadata<JsonData> hitsMetadata = innerHitsResult.hits();
+
+			if (hitsMetadata == null) {
+				continue;
+			}
+
+			List<Hit<JsonData>> hits = hitsMetadata.hits();
+
+			if (hits == null) {
+				continue;
+			}
+
+			for (Hit<JsonData> innerHit : hits) {
+				_populateHighlightFields(innerHit, highlightFields);
+			}
+		}
 	}
 
 	private Map<String, Object> _translateSource(JsonData jsonData) {
