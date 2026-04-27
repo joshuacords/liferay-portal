7ff66067b5cc588604483257c15061ac59dd9436
Joshua Cords <joshua.cords@liferay.com>
2026-04-27 15:55:45 -0700

    LPD-72159 object-service: refactor
    https://liferay.atlassian.net/browse/LPD-72159

--- modules/apps/object/object-service/src/main/java/com/liferay/object/internal/search/spi/model/index/contributor/ObjectEntryModelDocumentContributor.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/search/spi/model/index/contributor/ObjectEntryModelDocumentContributor.java
@@ -122,7 +122,7 @@ public class ObjectEntryModelDocumentContributor
 
 	private void _addLocalizedTitleFields(
 			Document document, ObjectDefinition objectDefinition,
-			ObjectEntry objectEntry, Map<String, Serializable> values)
+			ObjectEntry objectEntry)
 		throws Exception {
 
 		long titleObjectFieldId = objectDefinition.getTitleObjectFieldId();
@@ -138,6 +138,8 @@ public class ObjectEntryModelDocumentContributor
 
 			return;
 		}
+
+		Map<String, Serializable> values = objectEntry.getIndexedValues();
 
 		Map<String, Object> localizedValues = (Map<String, Object>)values.get(
 			titleObjectField.getI18nObjectFieldName());
@@ -480,12 +482,7 @@ public class ObjectEntryModelDocumentContributor
 
 		document.addKeyword("objectEntryId", objectEntry.getObjectEntryId());
 
-		if (values == null) {
-			values = objectEntry.getIndexedValues();
-		}
-
-		_addLocalizedTitleFields(
-			document, objectDefinition, objectEntry, values);
+		_addLocalizedTitleFields(document, objectDefinition, objectEntry);
 
 		ObjectFolder objectFolder = objectDefinition.getObjectFolder();
 
