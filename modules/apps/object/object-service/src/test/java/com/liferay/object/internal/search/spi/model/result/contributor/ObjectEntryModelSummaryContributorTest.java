ea4b50dca3ee9e58f59668cbd2e9505b79ac7c2b
Joshua Cords <joshua.cords@liferay.com>
2026-05-22 16:48:12 -0700

    LPD-75868 object-service: align summary fallback for title and content
    https://liferay.atlassian.net/browse/LPD-75868

--- modules/apps/object/object-service/src/test/java/com/liferay/object/internal/search/spi/model/result/contributor/ObjectEntryModelSummaryContributorTest.java
+++ modules/apps/object/object-service/src/test/java/com/liferay/object/internal/search/spi/model/result/contributor/ObjectEntryModelSummaryContributorTest.java
@@ -8,14 +8,19 @@ package com.liferay.object.internal.search.spi.model.result.contributor;
 import com.liferay.object.constants.ObjectEntrySearchConstants;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.search.Document;
 import com.liferay.portal.kernel.search.DocumentImpl;
 import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.search.Summary;
 import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.LocalizationUtil;
+import com.liferay.portal.language.LanguageImpl;
 import com.liferay.portal.test.rule.LiferayUnitTestRule;
+import com.liferay.portal.util.LocalizationImpl;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
@@ -29,6 +34,17 @@ public class ObjectEntryModelSummaryContributorTest {
 	@Rule
 	public static final LiferayUnitTestRule liferayUnitTestRule =
 		LiferayUnitTestRule.INSTANCE;
+
+	@Before
+	public void setUp() {
+		LanguageUtil languageUtil = new LanguageUtil();
+
+		languageUtil.setLanguage(new LanguageImpl());
+
+		LocalizationUtil localizationUtil = new LocalizationUtil();
+
+		localizationUtil.setLocalization(new LocalizationImpl());
+	}
 
 	@Test
 	public void testGetSummaryUsesLocalizedObjectEntryContentSnippet() {
