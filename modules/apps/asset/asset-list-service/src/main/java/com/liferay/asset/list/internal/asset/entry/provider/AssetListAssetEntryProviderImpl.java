3cb334e47e9603e8019f92dcd05660a39ff8658e
Olivia Yu <olivia.yu@liferay.com>
2026-08-24 10:41:12 -0700

    LPD-74731 LPD-103180 asset-list-service: Order by Field.MODIFIED_DATE
    AssetEntryQuery only accepts the column names listed in ORDER_BY_COLUMNS, so translate Field.MODIFIED_DATE back to "modifiedDate" on the way out of the provider instead of teaching the kernel a second spelling for the same column.

--- modules/apps/asset/asset-list-service/src/main/java/com/liferay/asset/list/internal/asset/entry/provider/AssetListAssetEntryProviderImpl.java
+++ modules/apps/asset/asset-list-service/src/main/java/com/liferay/asset/list/internal/asset/entry/provider/AssetListAssetEntryProviderImpl.java
@@ -271,10 +271,10 @@ public class AssetListAssetEntryProviderImpl
 
 		assetEntryQuery.setOrderByCol2(
 			_getOrderByColumn(
-				assetListEntry.getCompanyId(), "modifiedDate",
+				assetListEntry.getCompanyId(), Field.MODIFIED_DATE,
 				GetterUtil.getString(
 					unicodeProperties.getProperty(
-						"orderByColumn2", "modifiedDate"))));
+						"orderByColumn2", Field.MODIFIED_DATE))));
 
 		assetEntryQuery.setOrderByType1(
 			GetterUtil.getString(
@@ -981,7 +981,7 @@ public class AssetListAssetEntryProviderImpl
 		long companyId, String defaultOrderByColumn, String orderByColumn) {
 
 		if (!orderByColumn.startsWith(StringPool.OPEN_CURLY_BRACE)) {
-			return orderByColumn;
+			return _toAssetEntryQueryOrderByColumn(orderByColumn);
 		}
 
 		if (FeatureFlagManagerUtil.isEnabled(companyId, "LPD-74731")) {
@@ -990,7 +990,7 @@ public class AssetListAssetEntryProviderImpl
 		}
 
 		if (orderByColumn.startsWith(StringPool.OPEN_CURLY_BRACE)) {
-			return defaultOrderByColumn;
+			return _toAssetEntryQueryOrderByColumn(defaultOrderByColumn);
 		}
 
 		return orderByColumn;
@@ -1182,6 +1182,14 @@ public class AssetListAssetEntryProviderImpl
 				Comparator.comparing(
 					AssetListEntrySegmentsEntryRel::getPriority)),
 			AssetListEntrySegmentsEntryRel::getSegmentsEntryId);
+	}
+
+	private String _toAssetEntryQueryOrderByColumn(String orderByColumn) {
+		if (orderByColumn.equals(Field.MODIFIED_DATE)) {
+			return "modifiedDate";
+		}
+
+		return orderByColumn;
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
